54a19643d64dc70de6197606619e0a173a2977a6
Monika Wenger <wenger@fortiss.org>
2015-08-05 11:09:10 +0200

    fix header footer phps

--- content/header.php
+++ content/header.php
@@ -1,7 +1,7 @@
 <?php
-<!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5CMRMZ"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+echo "<!-- Google Tag Manager -->
+<noscript><iframe src='//www.googletagmanager.com/ns.html?id=GTM-5CMRMZ'
+height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
@@ -12,19 +12,19 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <header>
 	<table>
 		<tr>
-			<td><img src="img/4diac.png" height="80px" style="padding-left:8px"/></td>
-			<td><h1 class="cOrange">design once use everywhere</h1></td>
-			<td><h1 class="rDarkBlue">Framework for Industrial <br>Automation & Control</br></h1></td>
+			<td><img src='img/4diac.png' height='80px' style='padding-left:8px'/></td>
+			<td><h1 class='cOrange'>design once use everywhere</h1></td>
+			<td><h1 class='rDarkBlue'>Framework for Industrial <br>Automation & Control</br></h1></td>
 		</tr>
 	</table>
 </header>
 
 <nav>
 	<ul>
-		<li><a href="en_com.html">Community</a></li>
-		<li><a href="en_uws.html">User's Workshop</a></li>
-		<li><a href="en_dow.html">Download</a></li>
-		<li><a href="documentation/help.html">Documentation</a></li>
+		<li><a href='en_com.html'>Community</a></li>
+		<li><a href='en_uws.html'>User's Workshop</a></li>
+		<li><a href='en_dow.html'>Download</a></li>
+		<li><a href='documentation/help.html'>Documentation</a></li>
 	</ul>
-</nav>
+</nav>"
 ?>
