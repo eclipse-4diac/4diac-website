ec4861632cd92d600c3d473db28ccb49475ebfce
Monika Wenger <wenger@fortiss.org>
2015-08-05 15:31:01 +0200

    add news arrows

--- content/en_main.php
+++ content/en_main.php
@@ -14,27 +14,21 @@
 </section>
 
 <section class="news">
-	<!--canvas id="leftNewsArrow" width="80" height="80"/-->
-	<!--a href="" class="chevron" id="chevron"/-->
+	<a href="">
+		<svg width="60px" height="60px">
+			<polygon points="15,0 45,0 30,30 45,60 15,60 0,30 15,0" style="fill:rgba(0,137,209,1.0);fill-rule:evenodd;" />
+			Sorry, your browser does not support inline SVG.
+		</svg>
+	</a>
 	<a href="">6th 4DIAC <br>Users' Workshop</a>
 	<a href="">Visualize with <br>Eclipse SCADA</a>
 	<a href="">Tutorial <br>Videos</a>
-	<!--canvas id="rightNewsArrow" width="80" height="80"/-->
-	<script>
-	var c = document.getElementById("leftNewsArrow");
-var ctx = c.getContext("2d");
-ctx.fillStyle = "#0089d1";
-ctx.beginPath();
-ctx.moveTo(40,0);
-ctx.lineTo(60,0);
-ctx.lineTo(40,40);
-ctx.lineTo(60,80);
-ctx.lineTo(20,80);
-ctx.lineTo(0,40);
-ctx.lineTo(20,0);
-ctx.closePath();
-ctx.fill();
-	</script>
+	<a href="">
+		<svg width="60px" height="60px">
+			<polygon points="15,0 45,0 60,30 45,60 15,60 30,30 15,0" style="fill:rgba(0,137,209,1.0);fill-rule:evenodd;" />
+			Sorry, your browser does not support inline SVG.
+		</svg>
+	</a>
 </section>
 
 <section class="content">
