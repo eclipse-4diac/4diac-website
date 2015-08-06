8a3ad33a823aae27525a07494644e886a37c4016
Monika Wenger <wenger@fortiss.org>
2015-08-06 17:05:53 +0200

    add community content

--- content/en_com.php
+++ content/en_com.php
@@ -3,7 +3,7 @@
 <meta charset="ANSI">
 <link rel="stylesheet" type="text/css" href="format.css">
 
-<title>4DIAC-COM</title>
+<title>4DIAC-Community</title>
 </head>
 
 <body>
@@ -14,11 +14,88 @@
 </section>
 
 <section class="content">
-	<h1 class="cOrange">Community</h1>
-	<p>Committers and Mentors</p>	
-	<p>Forum</p>
-	<p>Mailing List</p>
-	<p>Contribute</p>
+	<h1>Community</h1>
+	<h2>Who's Involved</h2>
+	<p>
+		<b>Project Lead:</b> 
+		<ul>
+			<li>Alois Zoitl (fortiss GmbH)</li>
+		</ul>
+	</p>	
+	<p>
+		<b>Committers:</b>
+		<ul>
+			<li>Alois Zoitl (fortiss GmbH)</li>
+			<li>Gerhard Ebenhofer (Profactor GmbH)</li>
+			<li>Martin Melik-Merkumians (TU Wien ACIN)</li>
+			<li>Monika Wenger (fortiss GmbH)</li>
+		</ul>
+	</p>	
+	<p>
+		<b>Mentors:</b>
+		<ul>
+			<li>Ed Merks</li>
+			<li>Eike Stepper</li>
+		</ul>
+	</p>
+	
+	
+	<h2>Project Mailing List</h2>
+	<ul>
+		<li><a href="https://dev.eclipse.org/mailman/listinfo/4diac-dev">4diac-dev:</a> 4diac developer discussions</li>
+	</ul>
+	
+	<h2>Forum</h2>
+	<ul>
+		<li><a href="https://www.eclipse.org/forums/index.php/f/308/">eclipse</a></li>
+		<li><a href="http://sourceforge.net/p/fordiac/discussion/">sourceforge</a></li>
+	</ul>
+	
+	
+	<h2>Support</h2>
+	<p>The following commercial enhancements and services related to 4DIAC:</p>
+	<ul>
+		<li>FORTE porting to different embedded platforms and operating systems (e.g. VxWorks, eCOS, IPC@CHIP ...)</li>
+		<li>function block library development</li>
+		<li>FORTE customisation</li>
+		<li>Various export filters</li>
+		<li>4DIAC-IDE customisation (e.g. editing of IEC 61131-3 compliant Function Block Networks ...)</li>
+	</ul>
+	<p>In order to understand the 4DIAC concepts and the corresponding implementation it is advisable to get familiar with the IEC 61499 reference model. The following books provide some information about the IEC 61499 concept and the 4DIAC implementation:</p>
+		<ul>
+			<li><a href="http://www.amazon.de/Modelling-Control-Systems-Using-Engineering/dp/1849197601/ref=sr_1_1?ie=UTF8&qid=1438845779&sr=8-1&keywords=zoitl">Modelling Control Systems Using Iec 61499 by Robert W. Lewis and Alois Zoitl</a></li>
+			<li><a href="http://www.amazon.de/Real-Time-Execution-61499-Alois-Zoitl/dp/1934394270/ref=sr_1_3?ie=UTF8&qid=1438845779&sr=8-3&keywords=zoitl">Real-Time Execution for Iec 61499 by Alois Zoitl</a></li>
+			<li><a href="https://www.crcpress.com/Distributed-Control-Applications-Guidelines-Design-Patterns-and-Application/Zoitl-Strasser/9781482259056">Distributed Control Applications by Alois Zoitl and Thomas Strasser</a></li>
+		</ul>
+	
+	
+	<h2>Contribute</h2>
+	<p>If you want to support 4DIAC, there multiple ways to contribute.</p>	
+	<p><b>Documentation, Marketing, and Supporting new Users</b>
+		<ul>
+			<li>Improve and extend the existing documentation in the 4DIAC Wiki</li>
+			<li>Write new tutorials for the 4DIAC Wiki</li>
+			<li>Support the Forums: Answer questions in the different forums</li>
+			<li>Spread the word: link to 4DIAC, use 4DIAC logo on your web page, write articles, blog entries, etc.</li>
+		</ul>
+	</p>
+	<p><b>Bug Wrangling and Testing</b>
+		<ul>
+			<li>Find and report bugs to ensure the quality of 4DIAC.</li>
+			<li>Submit ideas for improvement via 4DIAc's issue tracker.</li>
+			<li>Review and verify reported bugs. Sometimes there is not enough info attached to a bug report or it is hard to reproduce. Trying to reproduce a bug and extending the description is of great value.</li>
+			<li>Rate Bugs, feature requests, and ideas. This helps that we can focus our limited resources on the most important elements first.</li>
+		</ul>
+	</p> 
+	<p><b>Development</b>
+		<br>If you are a developer feel free to grab the code and start hacking. You can submit your patch any time when you are ready.
+		<ul>
+			<li>Extend and improve FB library. Having a large set of FBs increases the usability of 4DIAC for developing control solutions.</li>
+			<li>For ideas where to start your effort have a look at the bugs and features submitted to the 4DIAC's issue tracker.</li>
+			<li>For getting started feel free to ask questions in the Developers forum.</li>
+		</ul>
+	</p>
+	
 </section>
 
 <?php include 'footer.html';?>
