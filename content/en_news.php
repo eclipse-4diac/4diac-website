a2c6136cd8117104e8bea44defb8e8766afce266
Monika <wenger@fortiss.org>
2015-08-12 10:32:03 +0200

    add news button to header

---
+++ content/en_news.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+<meta charset="ANSI">
+<link rel="stylesheet" type="text/css" href="format.css">
+
+<title>4DIAC-News</title>
+</head>
+
+<body>
+<?php include 'header.html';?>
+
+<section class="img">
+	<img src="img/news.png"/>
+</section>
+
+<section class="content">
+	<h1>News</h1>
+	
+</section>
+
+<?php include 'footer.html';?>
+
+</body>
+
+</html> 
