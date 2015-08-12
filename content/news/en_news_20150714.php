8405010d1daf70df0975a8d115fbff94bb2244eb
Monika <wenger@fortiss.org>
2015-08-12 12:27:18 +0200

    fix news entries, change header color

--- content/news/en_news_20150714.php
+++ content/news/en_news_20150714.php
@@ -1,5 +1,5 @@
-<div class="newsheader">Program for 6th 4DIAC Users' Workshop Available</div>
-<p>14-07-14 23:22 
+<div class="newsheader">New 4DIAC Logo</div>
+<p>14-07-15 23:22 
 	<br>Age: 29 days</p>
 <p>In order to reflect our transition to an Eclipse project we decided to update the 4DIAC logo. We now proudly present it here:</p>
 <img src="img/4diac.png"/>
--- content/news/en_news_20150715.php
+++ content/news/en_news_20150715.php
@@ -1,5 +1,5 @@
 <div class="newsheader">Program for 6th 4DIAC Users' Workshop Available</div>
-<p>14-07-15 22:35 
+<p>15-07-15 22:35 
 	<br>Age: 28 days</p>
 <p>In the last months we have started to cleanup 4DIAC's code base for the migration to the infrastructure provided by the Eclipse Foundation for their projects. The next step will be to rework the code to the naming conventions for eclipse projects. As this requires a complete restructuring and renaming of plug-ins, we will take our Mercurial repositories off-line on Friday July 17th.</p>
 <p>After the rework the code will be available in the new 4DIAC Git repository at Eclipse.org. We'll keep you updated and let you know when this is finished.</p>
