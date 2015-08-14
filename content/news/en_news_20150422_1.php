7edba238ddbd860f4048bc87a8490e1a1e150064
Monika Wenger <wenger@fortiss.org>
2015-08-14 11:06:41 +0200

    add previous news

---
+++ content/news/en_news_20150422_1.php
@@ -0,0 +1,26 @@
+<div id="20150422_1" class="newsheader">Eclipse SCADA visualising a process automated with 4DIAC: Download the demo!</div>
+<div class="news">
+	<span class="left"><?php $date="2015-04-22 14:04"; echo $date;?></span> 
+	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
+</div>
+<p><a href="#20141013">Back in October</a>, we said that we were creating an example for 4DIAC and Eclipse SCADA working together. This demo is now ready for download - and to make it worth the wait, it comes complete with a detailed step-by-step guide showing how to get it up and running yourself!</p>
+<p>The demo brings together Eclipse SCADA for the operator screen, 4DIAC for the controller, and the FBDK runtime for simulating the process. 4DIAC and Eclipse SCADA communicate via Eclipse SCADA's Small Footprint Protocol (SFP).</p>
+<p>You can <a href="https://www.youtube.com/watch?v=i9dqLrf7DGo">watch a recording of the demo</a> in action and read a bit more about what it does in <a href="#20141013">our previous post</a>. For the full story, read the <a href="http://download.fortiss.org/public/IA/FLCDemo/flc_demo_doc_20150420.pdf">documentation</a> with over 20 pages of details on</p>
+<ul>
+	<li>The individual parts of the demo and how they all fit together
+	<li>How to make FORTE - the 4DIAC runtime - run with SFP
+	<li>How to build an Eclipse SCADA VI (Visual Interface) client
+	<li>Where to find more valuable information about Eclipse SCADA
+	<li>Tips for development and troubleshooting
+</ul>
+<p>The complete source code for the demo is available for download (as are Eclipse SCADA, 4DIAC and the FBDK, of course). The <a href="http://download.fortiss.org/public/IA/FLCDemo/4diac_eclipsescada_flcdemo_20150420.zip">download package</a> also includes a precompiled Windows executable of FORTE with SFP support for your convenience. We are also planning to bundle the demo source code with 4DIAC in one of the upcoming releases.</p>
+<p>Your feedback is welcome!</p>
+<p>Ben Schneider
+	<br>fortiss GmbH
+	<br>schneibe (at) in.tum.de
+</p>
+<figure>
+	<img src="img/eclipseSCADA.jpg" alt="EclipseSCADA"/>
+	<figcaption>EclipseSCADA</figcaption>
+</figure>
+<?php include 'greeting.html';?>
--- content/news/en_news_20150623.php
+++ content/news/en_news_20150623.php
@@ -3,7 +3,7 @@
 	<span class="left"><?php $date="2015-06-15 22:21"; echo $date;?></span> 
 	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
 </div>
-<p>The full program of the upcoming 4DIAC Users' Workshop is now fixed and available at the workshop page. As in the last years we got very interesting submission and are pleased to announce five talks.</p>
+<p>The full program of the upcoming 4DIAC Users' Workshop is now fixed and available at the <a href="en_workshop_2015.php">workshop page</a>. As in the last years we got very interesting submission and are pleased to announce five talks.</p>
 <p>Furthermore we will have a set of posters showing further usage examples of 4DIAC.</p>
 <p>Looking forward to meet you at this great event in Luxembourg.</p>
 <?php include 'greeting.html';?>
