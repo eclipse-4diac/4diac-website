3137ceac0c3de68b2a2d0f28871bbb59bce1f997
Monika Wenger <wenger@fortiss.org>
2015-08-14 08:32:04 +0200

    calculate age test

--- content/news/en_news_20150729.php
+++ content/news/en_news_20150729.php
@@ -1,7 +1,7 @@
 <div class="newsheader">4DIAC Repositories put Off-line </div>
 <div class="news">
 	<span class="left">29-07-15 20:50</span>
-	<span class="right">Age: <?php echo 'date_diff(date_create("2015-07-29"), date(Y-m-d))'?> - 14 days</span>
+	<span class="right">Age: <?php echo date_diff(date_create("2015-07-29"), date(Y-m-d));?> - 14 days</span>
 </div>
 <p>Today we finally have put the 4DIAC repositories for our migration to Eclipse premises off-line. This was a little delayed as there where some issues on SourceForge.</p>
 <?php include 'greeting.html';?>
