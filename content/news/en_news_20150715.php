db652e4b4ae4f5c380bdfdac7be58184a723237a
Monika Wenger <wenger@fortiss.org>
2015-08-14 10:15:04 +0200

    add links to news page from the main page

--- content/news/en_news_20150715.php
+++ content/news/en_news_20150715.php
@@ -1,4 +1,4 @@
-<div class="newsheader">Program for 6th 4DIAC Users' Workshop Available</div>
+<div id="20150715" class="newsheader">Program for 6th 4DIAC Users' Workshop Available</div>
 <div class="news">
 	<span class="left"><?php $date="2015-07-15 22:35"; echo $date;?></span> 
 	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
--- content/news/en_news_20150729.php
+++ content/news/en_news_20150729.php
@@ -1,4 +1,4 @@
-<div class="newsheader">4DIAC Repositories put Off-line </div>
+<div id="20150729" class="newsheader">4DIAC Repositories put Off-line </div>
 <div class="news">
 	<span class="left"><?php $date="2015-07-29 20:50"; echo $date;?></span> 
 	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
