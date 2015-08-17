8c064f24216e20207d313a31c4c343b5a63ce9f4
Monika Wenger <wenger@fortiss.org>
2015-08-17 13:13:15 +0200

    add eclipse incubation egg

--- content/footer.php
+++ content/footer.php
@@ -1,11 +1,14 @@
-<footer>
-	<span class="left">&copy; 2007-<?php echo date("Y"); ?></span>
-	<span class="center">
-		<a href="https://www.youtube.com/channel/UCt0MlNo6Y7dXCZSDACXFi1g"><img src="img/appbar.youtube.png"/></a>
-		<a href=""><img src="img/appbar.social.twitter.png"/></a>
-		<a href="https://plus.google.com/communities/101972285570306852659"><img src="img/appbar.googleplus.png"/></a>
-		<a href="https://www.linkedin.com/grp/home?gid=6949091"><img src="img/appbar.social.linkedin.png"/></a>
-		<a href="https://www.xing.com/communities/groups/4diac-1076214"><img src="img/appbar.social.xing.png"/></a>
-	</span>
-	<span class="right"><a href="http://www.eclipse.org/"><img src="img/eclipse.png"/></a></span>
-</footer>
+<section style="position:relative">
+	<img src="img/Egg-incubation.png" style="position:absolute;right:8px;bottom:45px;z-index:-1;"/>
+	<footer style="position:absolute;bottom:0px;">
+		<span class="left">&copy; 2007-<?php echo date("Y"); ?></span>
+		<span class="center">
+			<a href="https://www.youtube.com/channel/UCt0MlNo6Y7dXCZSDACXFi1g"><img src="img/appbar.youtube.png"/></a>
+			<a href=""><img src="img/appbar.social.twitter.png"/></a>
+			<a href="https://plus.google.com/communities/101972285570306852659"><img src="img/appbar.googleplus.png"/></a>
+			<a href="https://www.linkedin.com/grp/home?gid=6949091"><img src="img/appbar.social.linkedin.png"/></a>
+			<a href="https://www.xing.com/communities/groups/4diac-1076214"><img src="img/appbar.social.xing.png"/></a>
+		</span>
+		<span class="right"><a href="http://www.eclipse.org/"><img src="img/eclipse.png"/></a></span>
+	</footer>
+</section>
