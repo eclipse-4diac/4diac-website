1b1c118f87b39a57cba72ba2d6113317ac72da64
Monika Wenger <wenger@fortiss.org>
2015-08-19 20:46:31 +0200

    add news entry and update news on main page

--- content/en_main.php
+++ content/en_main.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<html>
 <head>
 <link rel="stylesheet" type="text/css" href="format.css">
 
@@ -19,9 +19,9 @@
 			Sorry, your browser does not support inline SVG.
 		</svg>
 	</a>
-	<a href="en_news.php#en_news_20150623">6th 4DIAC <br>Users' Workshop</a>
-	<a href="en_news.php#en_news_20150422_2">Visualize with <br>Eclipse SCADA</a>
-	<a href="en_news.php#en_news_20150420">Tutorial <br>Videos</a>
+	<a href="en_news.php#en_news_20150819">New 4diac Webpage</a>
+	<a href="en_news.php#en_news_20150729">4DIAC Repositories put Off-line</a>
+	<a href="en_news.php#en_news_20150715">6th 4DIAC <br>Users' Workshop</a>
 	<a href="">
 		<svg width="60px" height="60px">
 			<polygon points="10,0 30,0 40,20 30,40 10,40 20,20 10,0" style="fill:rgba(1,34,105,1.0);fill-rule:evenodd;" />
