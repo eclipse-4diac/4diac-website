766511721889d74543a7cb02412a866a55379147
alois Zoitl <zoitl@fortiss.org>
2015-08-27 11:44:04 +0200

    moved stuff out of content to avoid redirect

--- index.php
+++ index.php
@@ -1,1 +1,51 @@
-<?php header('Location: http://www.eclipse.org/4diac/content/en_main.php'); ?>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="format.css">
+<title>4DIAC</title>
+</head>
+
+<body>
+<?php include 'header.html';?>
+
+<section class="image">
+	<h1>IEC 61499 Implementation for Distributed <br>Devices of the Next Generation</br></h1>
+</section>
+
+<section class="news">
+	<a href="">
+		<svg width="40px" height="40px">
+			<polygon points="10,0 30,0 20,20 30,40 10,40 0,20 10,0" style="fill:rgba(1,34,105,1.0);fill-rule:evenodd;" />
+			Sorry, your browser does not support inline SVG.
+		</svg>
+	</a>
+	<a href="en_news.php#en_news_20150819">New 4diac Webpage</a>
+	<a href="en_news.php#en_news_20150729">4DIAC Repositories put Off-line</a>
+	<a href="en_news.php#en_news_20150715">6th 4DIAC <br>Users' Workshop</a>
+	<a href="">
+		<svg width="60px" height="60px">
+			<polygon points="10,0 30,0 40,20 30,40 10,40 20,20 10,0" style="fill:rgba(1,34,105,1.0);fill-rule:evenodd;" />
+			Sorry, your browser does not support inline SVG.
+		</svg>
+	</a>
+</section>
+
+<section class="content">
+	<a href="en_rte.php">RTE
+		<img height="100px" src="img/rte-button.png"/>
+		<span>Runtime Environment</span></a>
+	<a href="en_ide.php">IDE
+		<img height="100px" src="img/ide-button.png"/>
+		<span>Development Environment</span></a>
+	<a href="en_lib.php">LIB
+		<img height="100px" src="img/lib-button.png"/>
+		<span>Library</span></a>
+	<a href="en_sys.php">SYS
+		<img height="100px" src="img/sys-button.png"/>
+		<span>Systems</span></a>
+</section>
+
+<?php include 'footer.php';?>
+
+</body>
+
+</html> 
