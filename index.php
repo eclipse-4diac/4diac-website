7e311b6ea7f95b0a1fa42ffbf888534b423b1efb
Alois Zoitl <alil@users.sf.net>
2015-09-01 23:02:49 +0200

    played with news bar formatting

--- index.php
+++ index.php
@@ -19,10 +19,10 @@
 			Sorry, your browser does not support inline SVG.
 		</svg>
 	</a>
-	<span><a href="en_news.php#en_news_20150901">One Week till this Year's 4DIAC Users' Workshop</a><span>
-  <span><a href="en_news.php#en_news_20150830">2nd Maintenance Release for <br/>4DIAC 1.7</a><span>
-	<span><a href="en_news.php#en_news_20150819">New 4diac Webpage</a><span>
-	<span><a href="en_news.php#en_news_20150729">4DIAC Repositories put Off-line</a><span>
+	<span><a href="en_news.php#en_news_20150901">One Week till this Year's 4DIAC Users' Workshop</a></span>
+  <span><a href="en_news.php#en_news_20150830">2nd Maintenance Release for <br/>4DIAC 1.7</a></span>
+	<span><a href="en_news.php#en_news_20150819">New 4diac Webpage</a></span>
+	<span><a href="en_news.php#en_news_20150729">4DIAC Repositories put Off-line</a></span>
 	<a href="">
 		<svg width="60px" height="60px">
 			<polygon points="10,0 30,0 40,20 30,40 10,40 20,20 10,0" style="fill:rgba(1,34,105,1.0);fill-rule:evenodd;" />
