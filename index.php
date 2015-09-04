a45dc14be5450c6826d1d3255a243e166a3ef94d
Monika Wenger <wenger@fortiss.org>
2015-09-04 09:52:46 +0200

    remove unused commands

--- index.php
+++ index.php
@@ -18,9 +18,9 @@
 			Sorry, your browser does not support inline SVG.
 		</svg>
 	</a>
-	<div class="news-item"><span></span><a href="en_news.php#en_news_20150901">One Week till this Year's 4DIAC Users' Workshop</a></div>
-  <div class="news-item"><span></span><a href="en_news.php#en_news_20150830">2nd Maintenance Release for <br/>4DIAC 1.7</a></div>
-	<div class="news-item"><span></span><a href="en_news.php#en_news_20150819">New 4diac Webpage</a></div>
+	<div class="news-item"><a href="en_news.php#en_news_20150901">One Week till this Year's 4DIAC Users' Workshop</a></div>
+  <div class="news-item"><a href="en_news.php#en_news_20150830">2nd Maintenance Release for <br/>4DIAC 1.7</a></div>
+	<div class="news-item"><a href="en_news.php#en_news_20150819">New 4diac Webpage</a></div>
 	<a href="">
 		<svg width="60px" height="60px">
 			<polygon points="10,0 30,0 40,20 30,40 10,40 20,20 10,0" style="fill:rgba(1,34,105,1.0);fill-rule:evenodd;" />
