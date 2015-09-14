ebe8410b90163cef25e34480663d940a44c4def9
Alois Zoitl <alil@users.sf.net>
2015-09-14 23:18:18 +0200

    started work on feateres

--- en_features.php
+++ en_features.php
@@ -1,19 +1,50 @@
 <html>
 <head>
 <?php include 'head.php';?>
-<title>FORTE - The 4DIAC runtime environment</title>
+<title>4DIAC Features</title>
 </head>
 
 <body>
 <?php include 'header.html';?>
 
-<section class="img">
-	<img src="img/rte.png"/>
-</section>
-
 <section class="content">
-	<h1>IEC 61499 Runtime Environment</h1>
-	<p>The 4DIAC runtime environment (4DIAC-RTE, FORTE) is a small portable implementation of an IEC 61499 runtime environment targeting small embedded control devices (16/32 Bit), implemented in C++. It supports online-reconfiguration of its applications and the real-time capable execution of all function block types provided by the IEC 61499 standard.</p>
+	<h1>Features</h1>
+
+<p>The 4DIAC project provides an industrial-grade open source infrastructure for distributed Industrial Process Measurement and Control Systems (IPMCS) based on the IEC 61499 standard. This infrastructure includes:<p/>
+
+
+	<table>
+		<tr>
+			<td>
+          <a href="en_rte.php">	RTE <img height="100px" src="img/rte-button.png" alt="4DIAC runtime environment (FORTE) provides a device indepent execution of IEC 61499 control application"/></a>
+			</td>
+			<td>
+        <h2><a href="en_rte.php">FORTE (4DIAC-RTE)/></h2>
+        <p>The 4DIAC runtime environment (4DIAC-RTE, FORTE) is a small portable implementation of an IEC 61499 runtime environment targeting small embedded control devices (16/32 Bit), implemented in C++. 
+           It supports online-reconfiguration of its applications and the real-time capable execution of all function block types provided by the IEC 61499 standard.</p>
+			</td>
+		</tr>
+	</table>
+
+
+    the Eclipse based engineering tool called 4DIAC-IDE,
+    the portable and efficient real-time capable runtime environment called FORTE,
+    libraries providing commonly need application functionality (4DIAC-LIB), and
+    selected sample applications showing the key features (4DIAC-Systems).
+
+
+	<a href="en_ide.php">IDE
+		<img height="100px" src="img/ide-button.png" alt="4DIAC-IDE provides a development environment for developing distributed industrial processes measurements and control systems"/>
+		<span>Development Environment</span></a>
+	<a href="en_lib.php">LIB
+		<img height="100px" src="img/lib-button.png" alt="4DIAC's function block library provides you with a set of reusable function blocks reducing the effort of application development"/>
+		<span>Library</span></a>
+	<a href="en_sys.php">SYS
+		<img height="100px" src="img/sys-button.png" alt="Examle systems showing the features of IEC 61499 and 4DIAC"/>
+		<span>Systems</span></a>
+
+
+	<p></p>
 	<p>FORTE supports all IEC 61131-3 edition 2 elementary data-types, structures, and arrays. It provides a scalable architecture which allows FORTE to  adapt to the needs of your application. Applications can consist of any IEC 61499 element as basic function blocks (BFBs), composite function blocks (CFBs), service interface function blocks (SIFBs), adapters and subapplications. For connections between function blocks FORTE uses automatic and save castings (e.g., INT -> REAL). FORTE provides a flexible communication infrastructure via so called communication layers.</p>
 
 	<table>
