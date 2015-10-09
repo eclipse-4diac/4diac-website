07f7e28334bac441759f99ebd678158102043c6e
Monika Wenger <wenger@fortiss.org>
2015-10-09 09:15:09 +0200

    reduce size, add 4th news entry to index page

--- index.php
+++ index.php
@@ -27,6 +27,7 @@
 	<div class="news-item"><span></span><a href="en_news.php#<?php echo basename($files[0],".php");?>"><?php $lines = file($files[0]); echo $lines[1];?></a></div>
   <div class="news-item"><span></span><a href="en_news.php#<?php echo basename($files[1],".php");?>"><?php $lines = file($files[1]); echo $lines[1];?></a></div>
 	<div class="news-item"><span></span><a href="en_news.php#<?php echo basename($files[2],".php");?>"><?php $lines = file($files[2]); echo $lines[1];?></a></div>
+	<div class="news-item"><span></span><a href="en_news.php#<?php echo basename($files[3],".php");?>"><?php $lines = file($files[3]); echo $lines[1];?></a></div>
 	<a href="">
 		<svg width="60px" height="60px">
 			<polygon points="10,0 30,0 40,20 30,40 10,40 20,20 10,0" style="fill:rgba(1,34,105,1.0);fill-rule:evenodd;" />
