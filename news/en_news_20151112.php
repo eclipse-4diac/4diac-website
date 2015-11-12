f9949a6487fe7009d49388cc5b2a365d6523e71f
Alois Zoitl <alois.zoitl@gmx.at>
2015-11-12 22:36:55 +0100

    news on moving forum

---
+++ news/en_news_20151112.php
@@ -0,0 +1,12 @@
+<div id="<?php echo basename(__FILE__,'.php');?>" class="newsheader">
+	Next Step in the Migration to Eclipse
+</div>
+<div class="news">
+	<span class="left"><?php $date="2015-11-12 22:45"; echo $date;?></span> 
+	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
+</div>
+<p>In our work to migrate 4DIAC to Eclipse the next step will be to move the forum. We kindly ask you to direct your questions on 4DIAC on the new Forum at: <a href="http://eclipse.org/forums/eclipse.4diac">http://eclipse.org/forums/eclipse.4diac</a>
+
+<p>However this does not mean that your existing messages will be lost. The existing form messages at SourceForge will be put into readonly mode on Friday Nov. 20Th and can still be searched and read.</p>
+
+<p>Looking forward to read you in our new forum!</p>
