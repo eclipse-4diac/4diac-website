118fef9effb7cb6db1501ac1664bc78e9014a379
Monika Wenger <wenger@fortiss.org>
2015-11-14 13:14:23 +0100

    fox path for news entry images

--- news/en_news_20150830.php
+++ news/en_news_20150830.php
@@ -3,7 +3,7 @@
 </div>
 <div class="news">
 	<span class="left"><?php $date="2015-08-30 19:00"; echo $date;?></span> 
-	<span class="center"><img src="../img/release.png" alt="release" width="70"/></span>
+	<span class="center"><img src="img/release.png" alt="release" width="70"/></span>
 	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
 </div>
 <p>We are proud to announce the second maintenance release for 4DIAC 1.7 providing a set of bug fixes to 4DIAC-IDE (7), and FORTE (1). </p>
--- news/en_news_20150901.php
+++ news/en_news_20150901.php
@@ -3,7 +3,7 @@
 </div>
 <div class="news">
 	<span class="left"><?php $date="2015-09-01 22:45"; echo $date;?></span> 
-	<span class="center"><img src="../img/uws.png" alt="release" height="90"/></span>
+	<span class="center"><img src="img/uws.png" alt="release" height="90"/></span>
 	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
 </div>
 <p>There is only one week left till this year's <a href="https://www.eclipse.org/4diac/en_workshop_2015.php" rel="nofollow">4DIAC Users' Workshop</a>. As in the last years we have a great program with presentations covering topics like cyber security, formal verification and how to manage external events in 4DIAC, to name only some examples. </p>
