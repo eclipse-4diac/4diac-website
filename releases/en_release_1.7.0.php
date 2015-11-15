21948c66c24abf3982bcfdfad676c557abb737ba
Monika Wenger <wenger@fortiss.org>
2015-11-15 23:13:11 +0100

    add release note dummies

---
+++ releases/en_release_1.7.0.php
@@ -0,0 +1,14 @@
+<p>In addition to the <a href="https://sourceforge.net/p/fordiac/issues/milestone/1.7/">1.7.0 issues</a> the following features are planned:</p>
+<ul>
+	<li>Service-Sequence Editor: 
+		<ul>
+		<li>...</li>
+		</ul>
+	</li>
+	<li>Improve UI:
+		<ul>
+		<li>Move functionality of interface tabs to property sheets</li>
+		</ul>
+	</li>
+</ul>
+</p>
---
+++ releases/en_release_1.8.0.php
@@ -0,0 +1,22 @@
+<p>In addition to the <a href="https://sourceforge.net/p/fordiac/issues/milestone/1.8/">1.8.0 issues</a> the following features are planned:
+<ul>
+	<li>Service-Sequence Editor: 
+		<ul>
+		<li>Connection Bug: Connection lines of new added input and output primitives (when an input primitive exists) start within the upper left corner</li>
+		</ul>
+	</li>
+	<li>Monitoring/Debugging:
+		<ul>
+		<li>Reactivate existing application breakpoint code</li>
+		<li>Monitoring of adapter values</li>		
+		</ul>
+	</li>
+	<li>Improve UI:
+		<ul>
+		<li>Move functionality of algorithm tab and internal variable tab to property sheets</li>
+		<li>Generate communication function blocks between function blocks mapped to different resources</li>
+		<li>Use Ed 2.0 Transition condition style</li>
+		</ul>
+	</li>
+</ul>
+</p>
