1d06acd4c23f007fa4d4ecd2b226f7990e7ce07f
Alois Zoitl <alil@users.sf.net>
2015-11-22 22:36:05 +0100

    updated contribution page

--- en_com.php
+++ en_com.php
@@ -15,43 +15,38 @@
 
 <section class="content">
 	<h1>Community</h1>
-	<h2>Support</h2>
+	<h2>Communication Channels</h2>
 	<table><tr>
-		<td class="h2twocollumntable">
-			<h3>Project Mailing List</h3>
-			<ul><li><a href="https://dev.eclipse.org/mailman/listinfo/4diac-dev">4diac-dev:</a> <br>4diac developer discussions</li></ul>
+		<td width="33%">
+			<h3>Discussion Forum</h3>
+         For asking questions and discussing 4DIAC related issues the <a href="http://eclipse.org/forums/eclipse.4diac">4DIAC disscion forum</a> is here for you.
 		</td>
-		<td class="h2twocollumntable">
-			<h3>Forum</h3>
-			<ul>
-				<li><a href="http://eclipse.org/forums/eclipse.4diac">eclipse</a></li>
-				<li><a href="http://sourceforge.net/p/fordiac/discussion/">sourceforge</a></li>
-			</ul>
+
+		<td width="33%">
+			<h3>Project Mailing List</h3>
+      Follow and participate in discussions on 4DIAC. You can <a href="https://dev.eclipse.org/mailman/listinfo/4diac-dev">subscribe here</a> 
+		</td>
+		<td width="33%">
+			<h3>Bugzilla</h3>
+      4DIAC bugs and enhancements are maintained using Bugzilla. Please submit issues and suggestions <a href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=4DIAC">here</a>.
 		</td>
 	</tr></table>
-	<p>The following commercial enhancements and services related to 4DIAC:</p>
-	<ul>
-		<li>FORTE porting to different embedded platforms and operating systems (e.g. VxWorks, eCOS, IPC@CHIP ...)</li>
-		<li>function block library development</li>
-		<li>FORTE customisation</li>
-		<li>Various export filters</li>
-		<li>4DIAC-IDE customisation (e.g. editing of IEC 61131-3 compliant Function Block Networks ...)</li>
-	</ul>
-	<p>In order to understand the 4DIAC concepts and the corresponding implementation it is advisable to get familiar with the IEC 61499 reference model. The following books provide some information about the IEC 61499 concept and the 4DIAC implementation:</p>
+  
+  	<p>In order to understand the 4DIAC concepts and the corresponding implementation it is advisable to get familiar with the IEC 61499 reference model. The following resources provide more information about the IEC 61499 concept and the 4DIAC implementation:</p>
 		<ul>
-			<li><a href="http://www.amazon.de/Modelling-Control-Systems-Using-Engineering/dp/1849197601/ref=sr_1_1?ie=UTF8&qid=1438845779&sr=8-1&keywords=zoitl">Modelling Control Systems Using Iec 61499 by Robert W. Lewis and Alois Zoitl</a></li>
-			<li><a href="http://www.amazon.de/Real-Time-Execution-61499-Alois-Zoitl/dp/1934394270/ref=sr_1_3?ie=UTF8&qid=1438845779&sr=8-3&keywords=zoitl">Real-Time Execution for Iec 61499 by Alois Zoitl</a></li>
+      <li><a href="https://en.wikipedia.org/wiki/IEC_61499">Wikipedia article on IEC 61499</a></li>
+			<li><a href="http://www.amazon.de/Modelling-Control-Systems-Using-Engineering/dp/1849197601/ref=sr_1_1?ie=UTF8&qid=1438845779&sr=8-1&keywords=zoitl">Modelling Control Systems Using IEC 61499 by Robert W. Lewis and Alois Zoitl</a></li>
+			<li><a href="http://www.amazon.de/Real-Time-Execution-61499-Alois-Zoitl/dp/1934394270/ref=sr_1_3?ie=UTF8&qid=1438845779&sr=8-3&keywords=zoitl">Real-Time Execution for IEC 61499 by Alois Zoitl</a></li>
 			<li><a href="https://www.crcpress.com/Distributed-Control-Applications-Guidelines-Design-Patterns-and-Application/Zoitl-Strasser/9781482259056">Distributed Control Applications by Alois Zoitl and Thomas Strasser</a></li>
 		</ul>
-	
-	
+
 	<h2>Contribute</h2>
-	<p>If you want to support 4DIAC, there are multiple ways to contribute.</p>	
+	<p>Thanks for your interest to support 4DIAC. There are multiple ways to contribute.</p>	
 	<h3>Documentation, Marketing, and Supporting new Users</h3>
   <p>
 		<ul>
-			<li>Improve and extend the existing documentation in the 4DIAC Wiki</li>
-			<li>Write new tutorials for the 4DIAC Wiki</li>
+			<li>Improve and extend the existing documentation</li>
+			<li>Write new tutorials</li>
 			<li>Support the forums: Answer questions in the different forums</li>
 			<li>Spread the word: link to 4DIAC, use 4DIAC logo on your web page, write articles, blog entries, etc.</li>
 		</ul>
@@ -60,7 +55,7 @@
   <p>
 		<ul>
 			<li>Find and report bugs to ensure the quality of 4DIAC.</li>
-			<li>Submit ideas for improvement via 4DIAC's issue tracker.</li>
+			<li>Submit ideas for improvement via <a href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=4DIAC">4DIAC's Bugzilla</a>.</li>
 			<li>Review and verify reported bugs. Sometimes there is not enough info attached to a bug report or it is hard to reproduce. Trying to reproduce a bug and extending the description is of great value.</li>
 			<li>Rate Bugs, feature requests, and ideas. This helps that we can focus our limited resources on the most important elements first.</li>
 		</ul>
@@ -72,8 +67,7 @@
 			<li>For ideas where to start your effort have a look at the bugs and features submitted to the 4DIAC's issue tracker.</li>
 			<li>For getting started feel free to ask questions in the Developers forum.</li>
 		</ul>
-	</p>
-	
+	</p>	
 
 	<h2>Who's Involved</h2>
 	<table>
