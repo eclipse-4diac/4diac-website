89b3c54d72c42a8d56948ae90de1f4b681e370f2
Alois Zoitl <zoitl@fortiss.org>
2015-11-23 17:42:37 +0100

    test

--- news/en_news_20151123.php
+++ news/en_news_20151123.php
@@ -2,7 +2,7 @@
 	4DIAC Enabled CONMELEON C1 PLC
 </div>
 <div class="news">
-	<span class="left"><?php $date="2015-10-18 22:30"; echo $date;?></span> 
+	<span class="left"><?php $date="2015-11-23 17:30"; echo $date;?></span> 
 	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
 </div>
 <p>Today we have a guest news entry presenting the CONMELEON C1 PLC:</p> 
