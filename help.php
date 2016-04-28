d63ebb286e6f11c8d89bd93de96dcc1fdbf7ad9b
Alois Zoitl <alil@users.sf.net>
2016-04-28 21:27:45 +0200

    fixed stuff in css

--- help.php
+++ help.php
@@ -11,7 +11,19 @@
 <section class="content">
 	<h1>Documentation</h1>
 
-<iframe id="documentationToc" src="documentation/toc.php"></iframe>
+<iframe id="documentationToc" >
+<?php 
+$XML = new DOMDocument(); 
+$XML->load( './documentation/toc.xml' ); 
+#echo $XML->saveXML();
+$xslt = new XSLTProcessor(); 
+$XSL = new DOMDocument(); 
+$XSL->load( './doctoc.xsl', LIBXML_NOCDATA); 
+#echo $XSL->saveXML();
+$xslt->importStylesheet( $XSL ); 
+print $xslt->transformToXML( $XML ); 
+?>
+</iframe>
 <iframe id="documentationContent" name="Content" src="documentation/html/overview/overview.html"  ></iframe>
 
 </section>
