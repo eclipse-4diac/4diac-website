f32eb3f74ee00d4deb7600640e9caf9a939ce2dc
alois Zoitl <zoitl@fortiss.org>
2016-04-28 16:06:16 +0200

    fixed ref problem

--- documentation/help.php
+++ documentation/help.php
@@ -1,13 +1,13 @@
 <html>
 <head>
-<?php include 'head.php';?>
+<?php include '../head.php';?>
 <title>4DIAC Documentation</title>
 <meta name="4DIAC documentation providing help and to get started with 4DIAC"> 
 <meta name="keywords" content="porting, tutorials, getting started, overview  ">
 </head>
 
 <body>
-<?php include 'header.html';?>
+<?php include '../header.html';?>
 
 
 <frameset cols="30%,70%">
@@ -19,6 +19,6 @@
   </noframes>
 </frameset>
 
-<?php include 'footer.php';?>
+<?php include '../footer.php';?>
 </body>
 </html> 
