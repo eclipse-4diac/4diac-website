d28248c7455f55622a6da64a08737f17f57e3dd7
alois Zoitl <zoitl@fortiss.org>
2016-04-28 16:49:58 +0200

    fixed ref problem

--- help.php
+++ help.php
@@ -19,11 +19,11 @@ chdir('documentation');
 echo getcwd() . "\n";
 
 echo '<frameset cols="30%,70%">';
-echo '<frame src="toc.php" name="Toc">';
+echo '<frame src="documentation/toc.php" name="Toc">';
 echo '<frame src="" name="Content">';
 	
 echo '<noframes>';
-echo '    <body><a href="toc.php">4DIAC help index</a></body>'; 
+echo '    <body><a href="documentation/toc.php">4DIAC help index</a></body>'; 
 echo '  </noframes>';
 echo '</frameset>';
 
