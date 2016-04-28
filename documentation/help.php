043490cb439a4317abb95b1bf3a4664c1b2c5826
alois Zoitl <zoitl@fortiss.org>
2016-04-28 16:03:43 +0200

    first test of a better frame for the documentation

---
+++ documentation/help.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<?php include 'head.php';?>
+<title>4DIAC Documentation</title>
+<meta name="4DIAC documentation providing help and to get started with 4DIAC"> 
+<meta name="keywords" content="porting, tutorials, getting started, overview  ">
+</head>
+
+<body>
+<?php include 'header.html';?>
+
+
+<frameset cols="30%,70%">
+  <frame src="toc.php" name="Toc">
+  <frame src="" name="Content">
+	
+  <noframes>
+    <body><a href="toc.php">4DIAC help index</a></body> 
+  </noframes>
+</frameset>
+
+<?php include 'footer.php';?>
+</body>
+</html> 
