f5ccad1c3a04bd6b479cf5f07628815fd2bb9631
Alois Zoitl <alil@users.sf.net>
2016-05-04 14:50:25 +0200

    added news entry for IEC 61499 training

---
+++ news/en_news_20160504.php
@@ -0,0 +1,19 @@
+<div id="<?php echo basename(__FILE__,'.php');?>" class="newsheader">
+	Seminar: Getting started with IEC 61499
+</div>
+<div class="news">
+	<span class="left"><?php $date="2016-05-05 14:50"; echo $date;?></span> 
+	<span class="center"><img src="img/uws.png" alt="release" height="90"/></span>
+	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
+</div>
+
+<p>As a part of the seminar series organized by the Bavarian Mechatronics & Automation Cluster, 
+fortiss offers a course introducing the IEC 61499 standard. 
+The full-day course (in German) will be held on June 9th in Munich. 
+It will give an overview of the models defined in IEC 61499 and how to apply them to the development 
+f distributed automation and control systems. 
+Further details and registration information can be found on the 
+<a href="http://www.cluster-ma.de/veranstaltungen/clusterveranstaltungen/events-details/index.html?tx_cwcmaeventmanager_pi1[event]=1256">
+web-page of the seminar</a>.</p> 
+
+<?php include 'greeting.html';?>
