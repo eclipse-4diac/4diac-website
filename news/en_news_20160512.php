52500ddee1b1c9e6f575a28cbe5736cd821e03e0
Alois Zoitl <alil@users.sf.net>
2016-05-12 18:03:25 +0200

    added news

--- news/en_news_20160512.php
+++ news/en_news_20160512.php
@@ -1,19 +1,23 @@
 <div id="<?php echo basename(__FILE__,'.php');?>" class="newsheader">
-	Seminar: Getting started with IEC 61499
+	Neon 4DIAC - Release Plans and Updates
 </div>
 <div class="news">
-	<span class="left"><?php $date="2016-05-05 14:50"; echo $date;?></span> 
-	<span class="center"><img src="img/uws.png" alt="release" height="90"/></span>
+	<span class="left"><?php $date="2016-05-12 16:30"; echo $date;?></span> 
 	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
 </div>
 
-<p>As a part of the seminar series organized by the Bavarian Mechatronics & Automation Cluster, 
-fortiss offers a course introducing the IEC 61499 standard. 
-The full-day course (in German) will be held on June 9th in Munich. 
-It will give an overview of the models defined in IEC 61499 and how to apply them to the development 
-f distributed automation and control systems. 
-Further details and registration information can be found on the 
-<a href="http://www.cluster-ma.de/veranstaltungen/clusterveranstaltungen/events-details/index.html?tx_cwcmaeventmanager_pi1[event]=1256">
-web-page of the seminar</a>.</p> 
+<p>Yesterday the 4DIAC committers decided that the next 4DIAC release (1.8) will be based on the upcoming <a href="https://wiki.eclipse.org/Neon/Simultaneous_Release_Plan">Eclipse Neon</a>. 
+Apart from the already implemented cool features like Lego Mindstorms EV3 support or the cleaned-up UI this will bring further 
+improvements in performance and memory usage of 4DIAC-IDE. 
+Those of you who are using 4DIAC-IDE from source will notice the change already when updating the source code from the repository.</p>
+
+<p>For this we will provide a first release candidate within the next two weeks. 
+The final release is planned shortly after the Eclipse Neon release on June 22nd.</p>
+
+<p>Please support us with testing the release candidates and providing us feedback on issues.</p>
 
 <?php include 'greeting.html';?>
+
+<p>P.S: The announced parallel code-clean up is also progressing well. 
+The UI model is already gone for good and many editors are already nearly fully functional again. 
+The next steps are to implement the new mapping model and further clean-up the data model.<br></p>
