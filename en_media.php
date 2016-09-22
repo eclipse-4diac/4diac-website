9e41effb92d2837db4ad71edacd0fbb57846a539
Alois Zoitl <alil@users.sf.net>
2016-09-22 17:21:06 +0200

    added new media section with a first set of images

---
+++ en_media.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<?php include 'head.php';?>
+<title>4DIAC Media</title>
+<meta name="description" content="Media like stickers desktop backgrounds, flyers for promoting 4diac."> 
+</head>
+
+<body>
+<?php include 'header.html';?>
+
+<section class="content">
+<h1>Media</h1>
+
+<h2>Showing that you are using 4diac</h2>	
+<p>With these two stickers you can show the world that you are using 4diac. Put it on your web-page, flyers, controllers, t-shirts... 
+
+<table><tr>
+		<td width="380" >
+		<img src="media/poweredby4diac_small.png" alt="release" height="100"/>
+		<br> <a href="media/poweredby4diac_small.svg">Powered by 4diac large SVG</a>
+		<br> <a href="media/poweredby4diac_small.png">Powered by 4diac large PNG</a>
+		</td>
+
+		<td width="380" style='padding:0px 30px 0px 0px'>
+		<img src="media/poweredby4diac_large.png" alt="release" height="100"/>
+		<br> <a href="media/poweredby4diac_large.svg">Powered by 4diac large SVG</a>
+		<br> <a href="media/poweredby4diac_large.png">Powered by 4diac large PNG</a>
+		</td>
+	</tr></table>
+
+
+
+<br/>
+
+
+</section>
+
+<?php include 'footer.php';?>
+
+</body>
+
+</html> 
