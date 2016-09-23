59330a7745b2131e33c7cf7fb66e1aacbc7d64e2
Alois Zoitl <zoitl@fortiss.org>
2016-09-23 10:25:44 +0200

    moved media to downloads

--- en_dow.php
+++ en_dow.php
@@ -560,6 +560,25 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 	include $files[0];
 }
 ?>
+
+<h2>Showing that you are using 4diac</h2>	
+<p>With these two stickers you can show the world that you are using 4diac. Put it on your web-page, flyers, controllers, laptops, t-shirts... 
+
+<table><tr>
+		<td width="380" >
+		<img src="media/poweredby4diac_small.png" alt="release" height="100"/>
+		<br> <a href="media/poweredby4diac_small.svg">Powered by 4diac large SVG</a>
+		<br> <a href="media/poweredby4diac_small.png">Powered by 4diac large PNG</a>
+		</td>
+
+		<td width="380" style='padding:0px 30px 0px 0px'>
+		<img src="media/poweredby4diac_large.png" alt="release" height="100"/>
+		<br> <a href="media/poweredby4diac_large.svg">Powered by 4diac large SVG</a>
+		<br> <a href="media/poweredby4diac_large.png">Powered by 4diac large PNG</a>
+		</td>
+	</tr></table>
+
+
 </section>
 <?php include 'footer.php';?>
 
