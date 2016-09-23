11ab7715f1ffb781102c0f4cbd01740fc6c6e5d6
Alois Zoitl <zoitl@fortiss.org>
2016-09-23 14:38:53 +0200

    added low resolution images for powered by logos to improve loading speed.

--- news/en_news_20160923.php
+++ news/en_news_20160923.php
@@ -6,11 +6,11 @@
 	<span class="center">
 	<table><tr>
 		<td>
-		<img src="media/poweredby4diac_small.png" alt="release" height="80"/>
+		<img src="media/poweredby4diac_small_lowres.png" alt="powered by 4diac small" height="80"/>
 		</td>
 
 		<td >
-		<img src="media/poweredby4diac_large.png" alt="release" height="80"/>
+		<img src="media/poweredby4diac_large_lowres.png" alt="powerd by 4diac large" height="80"/>
 		</td>
 	</tr></table></span>
 	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
