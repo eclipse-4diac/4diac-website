a3bdaa7291ff749aa06f44b9d7c4333ade542679
Alois Zoitl <alil@users.sf.net>
2016-10-13 13:48:39 +0200

    fixed date issue

--- news/en_news_20161012.php
+++ news/en_news_20161012.php
@@ -2,7 +2,7 @@
 	1<sup>st</sup> Maintenance Release for 4diac 1.8
 </div>
 <div class="news">
-	<span class="left"><?php $date="2015-10-12 14:00"; echo $date;?></span> 
+	<span class="left"><?php $date="2016-10-12 14:00"; echo $date;?></span> 
 	<span class="center"><img src="img/release.png" alt="release" width="70"/></span>	
 	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
 </div>
