f6a7974bc0c6450797a99ac212bd75cac95cf493
Alois Zoitl <alil@users.sf.net>
2016-11-17 17:21:00 +0100

    updated downlaod to 1.8.2 added news item

--- news/en_news_20161012.php
+++ news/en_news_20161012.php
@@ -1,29 +1,32 @@
 <div id="<?php echo basename(__FILE__,'.php');?>" class="newsheader">
-	1<sup>st</sup> Maintenance Release for 4diac 1.8
+	2<sup>nd</sup> Maintenance Release for 4diac 1.8
 </div>
 <div class="news">
-	<span class="left"><?php $date="2016-10-12 14:00"; echo $date;?></span> 
+	<span class="left"><?php $date="2016-11-17 19:00"; echo $date;?></span> 
 	<span class="center"><img src="img/release.png" alt="release" width="70"/></span>	
 	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
 </div>
-<p>We are proud to announce the first maintenance release for 4DIAC 1.8 providing a set of bug fixes to 4diac-ide, and FORTE.
+<p>We are proud to announce the second maintenance release for 4DIAC 1.8 providing a set of bug fixes to 4diac-ide, and FORTE.
 As always please download, test, and report problems and bugs.</p>
 <p>The main changes are:</p>
 <ul>
 	<li>4DIAC-IDE:
 		<ul>
-      		<li>Updated and restructuring the documentation</li>
-      		<li>Better handling of transition condition parsing when arrays are used in transition condition</li>
-      		<li>Cleaned-up and improved usability of action and algorithm editing</li>
-      		<li>Avoiding wrong use of unmap all</li>
-      		<li>Many smaller stabilization points</li>		
+      		<li>Improved transition condtion editing property sheet with code completion and syntax higlighting of condition expressions</li>
+      		<li>Fixed transition coditions without events to conform to IEC 61499-1 Ed. 2</li>
+      		<li>Fixed handling the array size of internal variables leading to wrong C++ code</li>
+      		<li>Correctly check for names of Algorithms, States, and internal variables</li>
+      		<li>Allow Algorithm editing also in the algorithm overview property sheet</li>
+      		<li>Null pointer exception when reordering actions</li>
+      		<li>Clean-up of algorithm editing and state editing property sheets</li>		
       	</ul>
 	</li>
 	<li>FORTE:
 		<ul>			
-       		<li>Fixed Modbus with no polling: client FB keeps on opening TCP connection</li>
-      		<li>Fixes in Odriod analogue IO support</li>
-      		<li>Fixes in EXPT function to make it more flexible to use in ST algorithms</li>
+       		<li>Fixed serial communication layer on win32 platform</li>
+      		<li>Fixed TOUPPER and TOLOWER string fucntion</li>
+      		<li>Fixed compiler warnings and cppcheck reports</li>
+      		<li>Fixed memory leak in Posix thread</li>
 		</ul>
 	</li>
 </ul>
