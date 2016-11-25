73f94a5ddb19a8de23d3d94dd37028fc61301b59
Monika Wenger <wenger@fortiss.org>
2016-11-25 14:24:49 +0100

    resized news img

--- news/en_news_20161125.php
+++ news/en_news_20161125.php
@@ -10,7 +10,7 @@
 <p>4diac reports back from a successful SPS/IPC/DRIVES attendance where it presented FORTE on an IndraControl XM22 PLC from Bosch Rexroth. A boiler controller application published its current values through MQTT to iot.eclipse.org, where the subscribed values have been visualized by MQTT-Spy.</p>
 
 <figure class="single">
-	<img src="img/SPSIPCDrives.png" width="300px" alt="4diac on IndraControl XM22 PLC from Bosch Rexroth and visualized with MQTT-Spy"/>
+	<img src="img/SPSIPCDrives.png" width="600px" alt="4diac on IndraControl XM22 PLC from Bosch Rexroth and visualized with MQTT-Spy"/>
 	<figcaption>4diac on IndraControl XM22 PLC from Bosch Rexroth and visualized with MQTT-Spy</figcaption>	
 </figure>
 
