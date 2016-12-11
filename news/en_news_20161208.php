891315d565e757144eb21ef55f15844c4d8fd60f
Alois Zoitl <alil@users.sf.net>
2016-12-11 23:10:32 +0100

    fixed typos in news

--- news/en_news_20161208.php
+++ news/en_news_20161208.php
@@ -7,7 +7,11 @@
 	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
 </div>
 
-<p>Eclipse 4diac™ was presented at the Eclipse DemoCamp in Munich on December 5TH. An introduction to its capabilities was followed by the demonstration of a 3D-printed prototype of a un/stacker module controlled by an IndraControl XM22 PLC from Bosch Rexroth. The PLC received the commands from an HMI and sent its states back using MQTT messages, allowing the user to see in real-time the process steps of the prototype</p>
+<p>Eclipse 4diac™ was presented at the Eclipse DemoCamp in Munich on December 5<sup>th</sup>. 
+An introduction to its capabilities was followed by the demonstration of a 3D-printed prototype of a un/stacker module controlled by an 
+IndraControl XM22 PLC from Bosch Rexroth. 
+The PLC received the commands from an HMI and sent its states back using MQTT messages, 
+allowing the user to see in real-time the process steps of the prototype.</p>
 
 <figure class="single">
 	<img src="news/figs/EclipseDemoCamp.png" width="600px" alt="4diac at Eclipse Demo Camp"/>
