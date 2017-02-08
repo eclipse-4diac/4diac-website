9d677624e21f20e04419dc118f6235f100ad5d39
Monika Wenger <wenger@fortiss.org>
2017-02-08 11:29:01 +0100

    news for 1.9.0.M1

--- news/en_news_20170202.php
+++ news/en_news_20170202.php
@@ -2,7 +2,7 @@
 	4<sup>th</sup> Maintenance Release for 4diac 1.8
 </div>
 <div class="news">
-	<span class="left"><?php $date="2016-12-22 12:00"; echo $date;?></span> 
+	<span class="left"><?php $date="2017-02-02 10:38"; echo $date;?></span> 
 	<span class="center"><img src="img/release.png" alt="release" width="70"/></span>	
 	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
 </div>
