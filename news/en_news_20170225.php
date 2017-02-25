56c21634d4517a99ca5a3b350950fb7e52539b55
Alois Zoitl <alil@users.sf.net>
2017-02-25 16:20:51 +0100

    fixed typo

--- news/en_news_20170225.php
+++ news/en_news_20170225.php
@@ -1,5 +1,5 @@
 <div id="<?php echo basename(__FILE__,'.php');?>" class="newsheader">
-	For those who missed it: 4diac Virtual Iot Meetup Video
+	For those who missed it: 4diac Virtual IoT Meetup Video
 </div>
 <div class="news">
 	<span class="left"><?php $date="2017-02-25 16:20"; echo $date;?></span> 
@@ -9,7 +9,6 @@
 On February 16<sup>th</sup> we gave a short overview on IEC 61499 and 4diac. If you missed the video of the talk is now online. </p>
 
 <?php include 'greeting.html';?>
-
-<iframe width="560" height="315" src="https://www.youtube.com/embed/1z3pxDngQG8" frameborder="0" allowfullscreen></iframe>
-
+<p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/1z3pxDngQG8" frameborder="0" allowfullscreen></iframe></p>
 <br>
