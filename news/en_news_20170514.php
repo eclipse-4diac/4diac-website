604110daf5fdded9ea01ec267b5edbba81f6250d
Alois Zoitl <alil@users.sf.net>
2017-05-14 20:18:22 +0200

    fixed some table props in the news

--- news/en_news_20170514.php
+++ news/en_news_20170514.php
@@ -8,14 +8,14 @@
 
 <table>
 	<tr>
-		<td>
+		<td width="66%" style='padding-right:10px;'>
 			<p>With our work on 4diac 1.9.0 we are proud to announce that FORTE now supports the uMIC.200 control devices from the company MicroControl. 
 			The uMIC.200 is a compact automation controller, which features in addtion to 8 digital IOs also several communication interfaces. 
 			Details can be found at the <a href="http://www.microcontrol.net/en/products/control-systems/umic200/">uMIC.200 website</a>.</p>
 
 			<p>For testing grab the latest FORTE version from the our <a href=" (http://git.eclipse.org/c/4diac/org.eclipse.4diac.forte.git)">Git repository</a>.</p>			
 		</td>
-		<td>
+		<td width="33%" style='padding-left:10px;'>
 			<div align="right">
 			<img alt="uMic.200" src="http://www.microcontrol.net/fileadmin/_processed_/csm_uMIC200_1200px_db13955608.png"><br>
 			<i>Source: MicroControl</i></div>
