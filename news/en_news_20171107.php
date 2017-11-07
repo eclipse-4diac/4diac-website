c1b628b899a34e19b5dc2064f2c2da107b8f8077
Alois Zoitl <alois.zoitl@gmx.at>
2017-11-07 17:23:55 +0100

    added news on jose as new committer

---
+++ news/en_news_20171107.php
@@ -0,0 +1,18 @@
+<div id="<?php echo basename(__FILE__,'.php');?>" class="newsheader">
+	Welcome to the New Committer Jose Cabral
+</div>
+<div class="news">
+	<span class="left"><?php $date="2017-11-07 17:25"; echo $date;?></span> 
+	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
+</div>
+
+<p>We would like to warmly welcome Jose Cabral to the committers' Team. 
+Over the last two years Jose has been increasingly contributing to 4diac. 
+He has been working mostly on the FORTE code, bringing it to new architectures, new modules and he's always worried about the cleanness of the code. 
+He's also responsible for the big change in the Documentation from last month.</p> 
+
+<p>We are sure his capabilities are a great extension to 4diac’s committer team.<p> 
+
+<p>Welcome Jose,
+	<br>The 4diac Team</p>
+<br>
