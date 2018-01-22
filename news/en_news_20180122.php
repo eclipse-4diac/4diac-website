85712356574b700ed3d7b8907aeab7c02bfe429d
Alois Zoitl <alois.zoitl@gmx.at>
2018-01-22 15:27:42 +0100

    fixed date in release news

--- news/en_news_20180122.php
+++ news/en_news_20180122.php
@@ -2,7 +2,7 @@
 	4diac 1.9.0 Milestone 3 Released!
 </div>
 <div class="news">
-	<span class="left"><?php $date="2017-12-20 07:45"; echo $date;?></span> 
+	<span class="left"><?php $date="2018-01-22 15:25"; echo $date;?></span> 
 	<span class="center"><img src="img/release.png" alt="release" width="70"/></span>	
 	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
 </div>
