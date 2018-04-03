53588075f3d4c04924c4aedb1c24ead9e1455a06
Alois Zoitl <alois.zoitl@gmx.at>
2018-04-03 10:20:40 +0200

    added news in cpselabs event

---
+++ news/en_news_20180403.php
@@ -0,0 +1,21 @@
+<div id="<?php echo basename(__FILE__,'.php');?>" class="newsheader">
+	4diac @ CPSE Labs Event &ldquo;Designing for Digital Transformation&rdquo;
+</div>
+<div class="news">
+	<span class="left"><?php $date="2018-04-03 10:20"; echo $date;?></span> 
+	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
+</div>
+
+<p>
+The European CPSE Labs project supported SMEs and mid-caps to experiment with new technologies like 4diac. 
+On April 19<sup>th</sup>, together with the Platforms4CPS project, results of the project as well as from the experiments will be presented in Munich.</p> 
+
+<p>This is a great chance to see how 4diac has been used in three CPSE Labs experiments, 
+to meet 4diac developers, and to hear about future funding possibilities for similar acitivties. 
+A detailed agenda and registration information can be found at the 
+<a href=”https://www.eventbrite.co.uk/e/cpse-labs-designing-for-digital-transformation-registration-43831110030”>event’s web-page</a>.
+</p>
+
+<p>Cheers,
+	<br>The 4diac Team</p>
+<br>
