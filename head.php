3074b3f500ab5c2d1797dbcabc70596d616f5c1b
Alois Zoitl <alois.zoitl@gmx.at>
2018-05-14 15:38:02 +0200

    update to themes tagmanager function

--- head.php
+++ head.php
@@ -9,15 +9,6 @@
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
 $App = new App();
 $Theme = $App->getThemeClass("quicksilver");
-if ($Theme->hasCookieConsent()) {
-    //Insert widgets from a 3rd party
-    echo  '<!-- Google Tag Manager -->';
-    echo  "<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':!";
-    echo  "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],";
-    echo  "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=";
-    echo  "'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);";
-    echo  "})(window,document,'script','dataLayer','GTM-5WLCZXC');</script>";
-    echo  "<!-- End Google Tag Manager -->";    
-}
+print $Theme->getGoogleTagManager();
 ?>
 
