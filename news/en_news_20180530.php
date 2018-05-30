e150015ba37cdd245f24fc0cc3f536fd64561f43
Alois Zoitl <alois.zoitl@gmx.at>
2018-05-30 17:48:26 +0200

    added post on vdma demonstrator

---
+++ news/en_news_20180530.php
@@ -0,0 +1,26 @@
+<div id="<?php echo basename(__FILE__,'.php');?>" class="newsheader">
+	Sneak Preview: 4diac and OPC UA in Action
+</div>
+<div class="news">
+	<span class="left"><?php $date="2018-05-30 17:30"; echo $date;?></span> 
+	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
+</div>
+
+<p>The <a href="https://ias.vdma.org/en/home">VDMA Integrated Assembly Solutions Group</a> is currently developing an OPC UA 
+Companion Specification for smart mechatronic components. 
+In order to show the potential and the applicability of this spec in action the group currently builds a demonstrator.</p>
+
+<p>We are very proud that 4diac is part of this demonstration machine implementing the new interaction and coordination mechanism. 
+See below a sneak preview on the machine consisting of roughly 30 smart mechatronic components. 
+Several modules in this machine will be programmed and operated with 4diac.</p>
+
+<p>Currently it is in the middle of its setup. You can see it live and in action at the 
+automatica trade fair in Munich on 19th - 22nd June, 
+<a href="https://automatica-munich.com/supporting-program/industry-4.0-demonstrators/opc-ua-demonstrator/index.html">hall B4, booth 332</a>.
+</p>
+
+<p>See you there,
+	<br>The 4diac Team</p>
+<p>
+<img src="news/figs/vdma_demonstrator.png" alt="VDMA Demonstrator setup sneak peak"/></p>
+<br>
