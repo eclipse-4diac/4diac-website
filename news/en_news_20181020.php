80b73c0a540cb3a547cc9949924f23f6a4509a56
Alois Zoitl <alois.zoitl@gmx.at>
2018-10-20 23:29:32 +0200

    fixed date in 4diac 1.10.0 rc1 release news

--- news/en_news_20181020.php
+++ news/en_news_20181020.php
@@ -2,7 +2,7 @@
 	4diac 1.10.0 Release Candidate 1 Published!
 </div>
 <div class="news">
-	<span class="left"><?php $date="2018-02-06 11:45"; echo $date;?></span> 
+	<span class="left"><?php $date="2018-10-20 23:25"; echo $date;?></span> 
 	<span class="center"><img src="img/release.png" alt="release" width="70"/></span>	
 	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
 </div>
