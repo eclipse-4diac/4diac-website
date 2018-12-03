44fb5deb489b6c07b662cf41b2ba617fffbabff5
Alois Zoitl <alois.zoitl@gmx.at>
2018-12-03 19:56:59 +0100

    added news on 4diac useability tests

---
+++ news/en_news_20181203.php
@@ -0,0 +1,24 @@
+<div id="<?php echo basename(__FILE__,'.php');?>" class="newsheader">
+	4diac Needs You!
+</div>
+<div class="news">
+	<span class="left"><?php $date="2018-12-03 19:53"; echo $date;?></span> 
+	<span class="center"></span>	
+	<span class="right">Age: <?php echo date_diff(date_create(date("Y-m-d h:i", strtotime($date))),date_create(date("Y-m-d h:i")))->format("%a days");?></span>
+</div>
+
+<p>As you know the Eclipse 4diac team has a strong focus in increasing 4diac's usability and user experience. 
+With the last releases we think we got better. But we know there is still a long road ahead of us.
+</p> 
+
+<p>For mastering that road we need your feedback!</p>
+
+<p>We have currently a questionnaire running where you can easily tell us where 4diac sucks or what is missing:</p> 
+<p style="text-align:center;"><b><a href="https://goo.gl/forms/hVivvJKgTbD9HciI3">Fill the 4diac IDE Evaluation Questionnaire</a></b></p>
+
+<p>However if you think questionnaires suck we are also looking for participants in usability tests:</p>
+<p style="text-align:center;"><b><a href="https://doodle.com/poll/ksfb2fat32zcx9zv">Register as Usability Tester!</a></b></p>
+
+<p>Thanks in advance for your support,
+	<br>The 4diac Team</p>
+<br>
