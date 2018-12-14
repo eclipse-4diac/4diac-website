59375ca462f045c843402acbef6ae7a9b99f1772
Alois Zoitl <alois.zoitl@gmx.at>
2018-12-14 20:57:00 +0100

    turned ordered lists into unorder

--- news/en_news_20181214.php
+++ news/en_news_20181214.php
@@ -9,19 +9,19 @@
 
 <p>We are currently in the middle of collecting feedback how we can improve 4diac. 
 To show you that we value your feedback, we can announce that already two smaller but really disturbing issues have been identified and fixed:</p>
-<ol>
+<ul>
     <li><a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=542674">Updating FBs lead to loss of connection layout information</a></li>
     <li><a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=542683">Updating Subapps was not possible</a></li>
-</ol>
+</ul>
     
 <p>These fixes will be available in the upcoming 1.10.1 maintenance release.</p>
 
 <p>We really need your feedback and there are several ways:</p>
-<ol>
+<ul>
     <li>Submit issues and suggestions through our <a href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=4DIAC"></a>bug tracker</li>
     <li>Fill out our anonymous <a href="https://goo.gl/forms/hVivvJKgTbD9HciI3">4diac IDE UX questionnaire</a></li>
     <li>Participate in our <a href="https://doodle.com/poll/ksfb2fat32zcx9zv">usability assessment</a></li>
-</ol>
+</ul>
 
 <p>Thanks in advance for your support,
 	<br>The 4diac Team</p>
