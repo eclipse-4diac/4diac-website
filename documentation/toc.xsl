571b0b1ac92bc3042a25465d3a95896895950e89
Alois Zoitl <alois.zoitl@gmx.at>
2018-12-17 22:57:50 +0100

    reworked help for better navigation

--- documentation/toc.xsl
+++ documentation/toc.xsl
@@ -7,39 +7,18 @@
 <xsl:output method="html" indent="no" encoding="ISO-8859-1"/>
 
 <xsl:template match="toc">
-   <html>
-<head>
-<base target="Content"/>
-<link rel="stylesheet" type="text/css" href="toc.css"/>
-<script type="text/javascript" src="../jquery-1.6.2.js"></script>
-<script type="text/javascript">
-	$(document).ready(function() {
-		$(".expand").click(function(e) {
-			$(this).toggleClass("expanded");
-			$(this).children("ul:first").slideToggle("300");
-			e.stopPropagation();
-		});	
-	}); //$(document).ready
-</script>	
-      </head>
-      <body>
-      <!--h2>4diac Help</h2-->         
-         <span><ul id="nav">
             <xsl:apply-templates />
-         </ul></span>
-      </body>
-   </html>
 </xsl:template>
 
 <xsl:template match="topic">
-   <li class="expand">
+   <li>
       <xsl:choose>
          <xsl:when test="@href">
-               <xsl:element name="a">
-               <xsl:attribute name="href">
-                  <xsl:value-of select="@href" />
-                  </xsl:attribute>
-               <xsl:value-of select="@label" />
+              <xsl:element name="a">
+               	<xsl:attribute name="href">
+               	  en_help.php?helppage=<xsl:value-of select="@href" />
+                </xsl:attribute>
+               	<xsl:value-of select="@label" />
             </xsl:element>
          </xsl:when>
          <xsl:otherwise>
--- en_help.php
+++ en_help.php
@@ -1,39 +1,100 @@
 <html>
 <head>
 <?php include 'head.php';?>
+<link rel="stylesheet" type="text/css" href="help.css">
 <title>4DIAC Documentation</title>
 <meta name="4DIAC documentation providing help and to get started with 4DIAC"> 
 <meta name="keywords" content="porting, tutorials, getting started, overview  ">
 
-<script language="javascript" type="text/javascript">
-function resizeIframe(obj, size) {
-    obj.style.height = size + 'px';
-    obj.style.overflow = "hidden";
-  }
+<script type="text/javascript" src="jquery-1.6.2.js"></script>
 
-  function resizeIframes() {
-    maxSize = document.getElementById("iframe-toc").contentWindow.document.body.scrollHeight;
-    if (maxSize < document.getElementById("iframe-content").contentWindow.document.body.scrollHeight)
-        maxSize = document.getElementById("iframe-content").contentWindow.document.body.scrollHeight;
-    resizeIframe(document.getElementById("iframe-toc"), maxSize);
-    resizeIframe(document.getElementById("iframe-content"), maxSize + 30);
-    resizeIframe(document.getElementById("documentation-content-section"), maxSize + 30);    
-  }
-</script>
 </head>
 
 <body>
 <?php include 'header.html'; ?>
 
-<section class="left">
-<iframe class="toc" id="iframe-toc" src="documentation/toc.php" onload="resizeIframes()" scrolling="no"></iframe>
+<section class="left" id="section-doc-toc">
+	<ul id="toc-nav">
+        <?php 
+            $XML = new DOMDocument(); 
+            $XML->load( 'documentation/html/toc.xml' ); 
+            #echo $XML->saveXML();
+            $xslt = new XSLTProcessor(); 
+            $XSL = new DOMDocument(); 
+            $XSL->load( 'documentation/toc.xsl', LIBXML_NOCDATA); 
+            #echo $XSL->saveXML();
+            $xslt->importStylesheet( $XSL ); 
+            echo $xslt->transformToXML( $XML ); 
+        ?>
+    </ul>
 </section>
 
+
 <section class="content">
-<h1>Documentation</h1>
-<section class="documentation" id="documentation-content-section">
-<iframe class="doc" id="iframe-content" name="Content" src="documentation/html/startHere/startHere.html" onload="resizeIframes()" scrolling="no"></iframe>
-</section>
+	<h1>Documentation</h1>
+	<section class="documentation">
+	    <div class ="doc">
+    	    <?php 
+    	    	$helpPage = 'documentation/html/startHere/startHere.html';
+    	    	
+    	    	if(isset($_GET['helppage'])) {
+    	    	    $helpPage = getHelpPageName($_GET['helppage'], $helpPage);
+    	    	}
+    	    	
+    	    	$helpContent = file_get_contents($helpPage);
+    	    	
+    	    	//fix internal links
+    	    	$helpContent = preg_replace('(<a href="[\./]*html/)', '<a href="en_help.php?helppage=html/', $helpContent);
+    	    	//fix image links
+    	    	$helpContent = preg_replace('( src="[\./]*html/)', ' src="documentation/html/', $helpContent);
+    	    	
+    	    	echo $helpContent;
+    	    	
+    	    	
+    	    	function getHelpPageName($helpFileName, $defaultHelpFile){
+    	    	    /* the checks in this functions are based on an example from 
+    	    	     * Jörg Reinholz at the https://wiki.selfhtml.org/wiki/PHP/Tutorials/Templates/Dateien_mit_include_nachladen
+    	    	     * page provided as public domain. 
+    	    	     */ 
+    	    	    $helpFileNameCopy = $helpFileName;  //create copy for later chekcs
+    	    	    
+     	    	    //given file name must not contain ../, /. (hidden unix file), url (://) 
+    	    	    $arForbiddenStrings = array('../', '/.', '://', '\\\\', '//' );
+    	    	    //replace forbiden strings with nothing
+    	    	    $helpFileName=str_replace($arForbiddenStrings, '', $helpFileName);
+    	    	    
+    	    	    if(! $helpFileName == $helpFileNameCopy){
+    	    	        //there where bad strings in the given param, return the default help file
+    	    	        return $defaultHelpFile;
+    	    	    }
+    	    	    
+    	    	    $forbiddenChars='/[^A-Za-z0-9_.\/*+:,-]/';
+    	    	    if ( preg_match($forbiddenChars, $helpFileName) ) {
+    	    	        return $defaultHelpFile;
+    	    	    }
+    	    	    
+    	    	    if ( $helpFileName && '.' == $helpFileName{0} ) {
+    	    	        return $defaultHelpFile;
+    	    	    }
+    	    	    
+    	    	    //only allow a help file if it is also referenced in the toc
+    	    	    if(!strpos(file_get_contents("documentation/html/toc.xml"), $helpFileName)){
+    	    	        return $defaultHelpFile;
+    	    	    }
+    	    	    
+    	    	    $helpFileName = 'documentation/' . $helpFileName;
+    	    	    
+    	    	    if ( ! is_file($helpFileName ) || ! is_readable($helpFileName) ) {
+    	    	        return $defaultHelpFile;
+    	    	    }
+    	    	    
+    	    	    return $helpFileName;
+    	    	}
+    	    	
+    	    	
+    	    ?>
+	    </div>	    
+	</section>
 </section>
 
 <?php include 'footer.php'; ?>
