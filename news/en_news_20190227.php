2b9174b49b01737ebc6ef8c4c7033e6517fd7860
Alois Zoitl <alois.zoitl@gmx.at>
2019-02-27 20:53:44 +0100

    fixed typo in last news

--- news/en_news_20190227.php
+++ news/en_news_20190227.php
@@ -1,6 +1,6 @@
-<?php printNewsEntryHeader(__FILE__, "See 4diac live at Embedded World Traid Fair"); ?>
+<?php printNewsEntryHeader(__FILE__, "See 4diac live at Embedded World Trade Fair"); ?>
 
-<p>DH electronics presents at the Embedded World Traid Fair their new <a href="https://www.linkedin.com/pulse/multiprotocoll-iot-edge-computer-voice-controlled-plc-daxenberger/">"Multiprotocoll IoT Edge Computer with voice-controlled PLC and Cloud connection"</a> product.
+<p>DH electronics presents at the Embedded World trade fair their new <a href="https://www.linkedin.com/pulse/multiprotocoll-iot-edge-computer-voice-controlled-plc-daxenberger/">"Multiprotocoll IoT Edge Computer with voice-controlled PLC and Cloud connection"</a> product.
 This product features Eclipse 4diac as PLC environment. 
 See it live in <a href="https://www.embedded-world.de/de/ausstellerprodukte/embwld19/aussteller-38826704/dh-electronics-gmbh">hall 2 booth 100</a>! 
 </p>
