3fe59e6060ce2ae9c86c565bdb09e245ec8b20a1
Alois Zoitl <alois.zoitl@gmx.at>
2019-02-28 08:48:06 +0100

    added news on gsoc

---
+++ news/en_news_20190228.php
@@ -0,0 +1,22 @@
+<?php printNewsEntryHeader(__FILE__, "We Want You to Participate in Google Summer of Code "); ?>
+
+<p>You are a student currently working or planning to work with 4diac and looking for a summer project? 
+We have something for you! 
+4diac is participating in Google Summer of Code 2019. 
+This means that Google may sponsors your project with 4diac.</p>
+
+<p>We got you interested? Then please consider the following points:</p>
+<ul>
+	<li>Get in contact with us to discuss potential project topics on our <a href="https://www.eclipse.org/mailman/listinfo/4diac-dev">mailing list</a></li>
+	<li>For inspiration we compiled <a href="https://wiki.eclipse.org/Google_Summer_of_Code_2019_Ideas#Eclipse_4diac">first project ideas</a></li>
+	<li>See the official <a href="https://summerofcode.withgoogle.com/">Google Summer of Code web-page</a> for a detailed description of how to apply</li>
+</ul>
+
+<p>Looking forward working together with you!</p>
+
+<?php include 'greeting.html';?>
+
+
+
+
+
