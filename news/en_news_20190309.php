cc4aaebb404057907626854026977449435abf06
Monika Wenger <wenger@fortiss.org>
2019-03-09 14:31:02 +0100

    fix iac news

--- news/en_news_20190309.php
+++ news/en_news_20190309.php
@@ -7,8 +7,8 @@
 <p>The second demonstrator shows 4diac's TSN capabilities by sending two streams, a prioritized TSN stream and a non-prioritized best-effort stream, between two Beaglebone Black devices via 3 TTTech Akro 6/0 TSN switches. All devices have a common time base, which is achieved by the Precision Time Protocol (IEEE1588 - PTP). The prioritized stream is synchronized to a specific sending time point. The demonstrator shows on the one hand how 4diac uses the deterministic features of TSN, which means that the transmission of packets can be guaranteed and on the other hand that synchronizing the application and network schedule reduces latency and jitter of the transmitted packets.</p>
 
 <figure>
-	<img src="news/figs/TSN.png" width="440px" alt="TSN demonstrator"/>
-	<img src="news/figs/skill.png" width="340px" alt="OPC UA demonstrator"/>
+	<img src="news/figs/TSN.jpg" width="440px" alt="TSN demonstrator"/>
+	<img src="news/figs/Skills.jpg" width="340px" alt="OPC UA demonstrator"/>
 	<figcaption>Demonstrators at the Inspire Automation Camp, left TSN demonstrator, right OPC UA demonstrator</figcaption>	
 </figure>
 
