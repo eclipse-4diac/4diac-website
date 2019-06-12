68cf3386771531b367e82b2544327c4f3c767df8
Jose Cabral <cabral@fortiss.org>
2019-06-12 10:07:41 +0200

    Add news about 4diac in Gothenburg

---
+++ news/en_news_20190612.php
@@ -0,0 +1,9 @@
+<?php printNewsEntryHeader(__FILE__, " Arrowhead Framework support in 4diac"); ?>
+
+<p>Our powered by 4diac traffic light demonstrator reached Gothenburg to take part in the workshop of the Productive 4.0 project. The 4diac application can now communicate with the Arrowhead Framework over HTTP REST and/or OPC UA.</p>
+
+<img src="news/figs/20190612_arrowheadWithOPCUAAtGothenburg.jpg" width="800"/>
+
+<p>The documentation about how to use this amazing library in 4diac can be found <a href="https://www.eclipse.org/4diac/en_help.php?helppage=html/communication/arrowhead.html">here</a>. You can easily register your own services and look for orchestration rules for your devices.</p>
+
+<?php include 'greeting.html';?>
