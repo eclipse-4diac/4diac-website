9b8ef924d0355883081b4c5b422500eebcd0e976
Alois Zoitl <alois.zoitl@gmx.at>
2019-07-16 21:42:08 +0200

    added news on midih

---
+++ news/en_news_20190716.php
@@ -0,0 +1,16 @@
+<?php printNewsEntryHeader(__FILE__, "The European network MIDIH Supports Your Experiments with Eclipse 4diac"); ?>
+
+<p>We would like to draw your attention to a funding opportunity for experiments around Eclipse 4diac.</p>
+ 
+<p>The <a href="https://midih.eu/">European network MIDIH</a> supports selected application and technology experiments around Big Data with a reference architecture based on a 
+stack of proven technologies (including Eclipse 4diac) with up to 60 000 € funding. 
+Individual applicants can - once again - submit applications until 06.08. 
+Topics can, for example be, additive manufacturing, inter- and intralogistics, modelling/simulation, VR/AR and AI/ML.</p>
+ 
+<p>Further information is available at <a href="https://midih.eu/opencall_2.php">https://midih.eu/opencall_2.php</a></p>
+
+<?php include 'greeting.html';?>
+
+
+
+
