93a32e0dfa3f2514e7c44f980b96a41bcd49c231
Alois Zoitl <alois.zoitl@gmx.at>
2019-08-28 23:13:32 +0200

    [550536] Split support and contributing
    Furthermore some minor improvement and clen-ups.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=550536
    Change-Id: Id1921dc8a8f753ac9e6393159523768f340305f8
    Signed-off-by: Alois Zoitl <alois.zoitl@gmx.at>

--- en_com.php
+++ en_com.php
@@ -3,7 +3,7 @@
 <?php include 'head.php';?>
 <title>4diac-Support</title>
 <meta name="4diac open source community helping new users applying 4diac for their applications as well as driving the further development of 4diac."> 
-<meta name="keywords" content="open source community, mailing lists, forums, user support, how to contribute">
+<meta name="keywords" content="open source community, mailing lists, forums, user support">
 </head>
 
 <body>
@@ -25,7 +25,10 @@
 	<table><tr>
 		<td width="253" >
 			<h3>Discussion Forum</h3>
-         For asking questions and discussing 4diac related issues the <a href="http://eclipse.org/forums/eclipse.4diac">4diac discussion forum</a> is here for you.
+         	For asking questions and discussing 4diac related issues the <a href="http://eclipse.org/forums/eclipse.4diac">4diac discussion forum</a> is here for you.
+         
+         	<h3>Chat</h3>
+         	For a more interactive inter-action please try our <a href="https://mattermost.eclipse.org/eclipse/channels/4diac">Mattermost channel</a>.
 		</td>
 
 		<td width="253" style='padding:0px 30px 0px 30px'>
@@ -39,64 +42,17 @@
 		</td>
 	</tr></table>
   
-  	<p>In order to understand the 4diac concepts and the corresponding implementation it is advisable to get familiar with the IEC 61499 reference model. The following resources provide more information about the IEC 61499 concept and the 4diac implementation:</p>
+  	<h2>Background</h2>
+  	<p>In order to understand the modelling concepts used in Eclipse 4diac it is advisable to get familiar with the IEC 61499 reference model. The following resources provide more information about the IEC 61499 concept and the 4diac implementation:</p>
 		<ul>
-      <li><a href="https://en.wikipedia.org/wiki/IEC_61499">Wikipedia article on IEC 61499</a></li>
+      		<li><a href="https://en.wikipedia.org/wiki/IEC_61499">Wikipedia article on IEC 61499</a></li>
+      		<li><a href="en_help.php?helppage=html/before4DIAC/iec61499.html">Introduction to IEC 61499 in the Eclipse 4diac documentation</a>
 			<li><a href="http://www.amazon.de/Modelling-Control-Systems-Using-Engineering/dp/1849197601/ref=sr_1_1?ie=UTF8&qid=1438845779&sr=8-1&keywords=zoitl">Modelling Control Systems Using IEC 61499 by Robert W. Lewis and Alois Zoitl</a></li>
 			<li><a href="http://www.amazon.de/Real-Time-Execution-61499-Alois-Zoitl/dp/1934394270/ref=sr_1_3?ie=UTF8&qid=1438845779&sr=8-3&keywords=zoitl">Real-Time Execution for IEC 61499 by Alois Zoitl</a></li>
 			<li><a href="https://www.crcpress.com/Distributed-Control-Applications-Guidelines-Design-Patterns-and-Application/Zoitl-Strasser/9781482259056">Distributed Control Applications by Alois Zoitl and Thomas Strasser</a></li>
 		</ul>
+    <br>
 
-	<h2>Contribute</h2>
-	<p>Thanks for your interest to support 4diac. There are multiple ways to contribute.</p>	
-	<h3>Documentation, Marketing, and Supporting new Users</h3>
-		<ul>
-			<li>Improve and extend the existing documentation</li>
-			<li>Write new tutorials</li>
-			<li>Support the forums: Answer questions in the different forums</li>
-			<li>Spread the word: link to 4diac, use 4diac logo on your web page, write articles, blog entries, etc.</li>
-		</ul>
-	<h3>Bug Wrangling and Testing</h3>
-		<ul>
-			<li>Find and report bugs to ensure the quality of 4diac.</li>
-			<li>Submit ideas for improvement via <a href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=4DIAC">4diac's Bugzilla</a>.</li>
-			<li>Review and verify <a href="https://bugs.eclipse.org/bugs/buglist.cgi?list_id=13385396&product=4DIAC&query_format=advanced">reported bugs</a>. Sometimes there is not enough info attached to a bug report or it is hard to reproduce. Trying to reproduce a bug and extending the description is of great value.</li>
-			<li>Rate Bugs, feature requests, and ideas. This helps that we can focus our limited resources on the most important elements first.</li>
-		</ul>
-	<h3>Development</h3>
-  <p>If you are a developer feel free to grab the code and start hacking. You can submit your patch any time when you are ready.</p>
-		<ul>
-			<li>Extend and improve FB library. Having a large set of FBs increases the usability of 4diac for developing control solutions.</li>
-			<li>For ideas where to start your effort have a look at the bugs and features submitted to the <a href="https://bugs.eclipse.org/bugs/buglist.cgi?list_id=13385396&product=4DIAC&query_format=advanced">4diac's issue tracker</a>.</li>
-			<li>For getting started feel free to ask questions in the <a href="http://eclipse.org/forums/eclipse.4diac">4diac discussion forum</a> and in <a href="https://dev.eclipse.org/mailman/listinfo/4diac-dev">4diac's mailing list</a>.</li>
-		</ul>
-		
-
-	<!--h2>Who's Involved</h2>
-	<table>
-		<tr>
-			<td width="33%"><p>
-				<h3>Project Lead:</h3> 
-				<ul><li>Alois Zoitl (fortiss GmbH)</li></ul>
-			</p></td>
-			<td width="46%"><p>
-				<h3>Committers:</h3>
-				<ul>
-					<li>Alois Zoitl (fortiss GmbH)</li>
-					<li>Gerhard Ebenhofer (Profactor GmbH)</li>
-					<li>Martin Melik-Merkumians (TU Wien ACIN)</li>
-					<li>Monika Wenger (fortiss GmbH)</li>
-				</ul>
-			</p></td>	
-			<td width="20%"><p>
-				<h3>Mentors:</h3>
-				<ul>
-					<li>Ed Merks</li>
-					<li>Eike Stepper</li>
-				</ul>
-			</p></td>
-		</tr>
-	</table-->
 </section>
 
 <?php include 'footer.php';?>
---
+++ en_contribute.php
@@ -0,0 +1,79 @@
+<html>
+<head>
+<?php include 'head.php';?>
+<title>Contributing to Eclipse 4diac</title>
+<meta name="As open source project depends on the support of its userser and contributors. Learn hear how you can contribute to Eclispe 4diac"> 
+<meta name="keywords" content="how to contribute, develop new featuers, fix bugs, triage bugs, extend documentation, dvelop demo applications">
+</head>
+
+<body>
+<?php include 'header.html';?>
+
+<section class="img">
+	<img src="img/community.png"/>
+</section>
+
+<section class="left">
+ <!-- currently empty left section -->
+</section>
+
+
+
+<section class="content">
+	<h1>Contribute</h1>
+	<p>Thanks for your interest to support 4diac. There are multiple ways to contribute.</p>	
+	<h2>Documentation, Marketing, and Supporting new Users</h2>
+		<ul>
+			<li>Improve and extend the existing documentation</li>
+			<li>Write new tutorials</li>
+			<li>Support the forums: Answer questions in the different forums</li>
+			<li>Spread the word: link to 4diac, use 4diac logo on your web page, write articles, blog entries, etc.</li>
+		</ul>
+	<h2>Bug Wrangling and Testing</h2>
+		<ul>
+			<li>Find and report bugs to ensure the quality of 4diac.</li>
+			<li>Submit ideas for improvement via <a href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=4DIAC">4diac's Bugzilla</a>.</li>
+			<li>Review and verify <a href="https://bugs.eclipse.org/bugs/buglist.cgi?list_id=13385396&product=4DIAC&query_format=advanced">reported bugs</a>. Sometimes there is not enough info attached to a bug report or it is hard to reproduce. Trying to reproduce a bug and extending the description is of great value.</li>
+			<li>Rate Bugs, feature requests, and ideas. This helps that we can focus our limited resources on the most important elements first.</li>
+		</ul>
+	<h2>Development</h2>
+  		<p>If you are a developer feel free to grab the code and start hacking. You can submit your patch any time when you are ready.</p>
+		<ul>
+			<li>Extend and improve FB library. Having a large set of FBs increases the usability of 4diac for developing control solutions.</li>
+			<li>For ideas where to start your effort have a look at the bugs and features submitted to the <a href="https://bugs.eclipse.org/bugs/buglist.cgi?list_id=13385396&product=4DIAC&query_format=advanced">4diac's issue tracker</a>.</li>
+			<li>For getting started feel free to ask questions in the <a href="http://eclipse.org/forums/eclipse.4diac">4diac discussion forum</a> and in <a href="https://dev.eclipse.org/mailman/listinfo/4diac-dev">4diac's mailing list</a>.</li>
+		</ul>
+		<br>	
+
+	<!--h2>Who's Involved</h2>
+	<table>
+		<tr>
+			<td width="33%"><p>
+				<h3>Project Lead:</h3> 
+				<ul><li>Alois Zoitl (fortiss GmbH)</li></ul>
+			</p></td>
+			<td width="46%"><p>
+				<h3>Committers:</h3>
+				<ul>
+					<li>Alois Zoitl (fortiss GmbH)</li>
+					<li>Gerhard Ebenhofer (Profactor GmbH)</li>
+					<li>Martin Melik-Merkumians (TU Wien ACIN)</li>
+					<li>Monika Wenger (fortiss GmbH)</li>
+				</ul>
+			</p></td>	
+			<td width="20%"><p>
+				<h3>Mentors:</h3>
+				<ul>
+					<li>Ed Merks</li>
+					<li>Eike Stepper</li>
+				</ul>
+			</p></td>
+		</tr>
+	</table-->
+</section>
+
+<?php include 'footer.php';?>
+
+</body>
+
+</html> 
