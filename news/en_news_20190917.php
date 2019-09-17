8c6479ee8833e9420c42a3b35dc8d4a0d38f78a0
Alois Zoitl <alois.zoitl@gmx.at>
2019-09-17 14:46:02 +0200

    added news on 4days of 4diac

---
+++ news/en_news_20190917.php
@@ -0,0 +1,18 @@
+<?php printNewsEntryHeader(__FILE__, "Announcing the 4days of 4diac Summit", "img/uws.png"); ?>
+
+<p>Over the last 12 years Eclipse 4diac has grown into the open source PLC solution, with users and contributors all over the world. 
+Up to now we have mostly interacted through our forum, mailing list and Mattermost channel. 
+With the 4days of 4diac summit we would like to change that, and bring you all together in one place.</p> 
+
+<p>Therefore, we invite you all to Linz from November 11 till November 14 to discuss and program with each other on new 4diac topics and applications.</p>
+
+<p>We wanted to make the event as low barrier as possible: 
+So we provide the meeting rooms and coffee, and you bring your time and ideas. 
+Our goal is turn this event into 4days of showing, discussing, planning, hacking, experimenting, learning with Eclipse 4diac.</p>
+
+<p>4 days for the 4diac community to meet and exchange! 
+For details on location, registration and programm please visit the <a href="https://www.eclipse.org/4diac/en_eve.php?event=2019_4days">4days of 4diac page</a>.</p>
+
+<p>We are looking forward to meet you in Linz,
+<br>The 4diac Team</p>
+<br>
