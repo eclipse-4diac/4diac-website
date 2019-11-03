9a82e7ccfb6ef90fa5c148fa81f324c293ba83fb
Alois Zoitl <alois.zoitl@gmx.at>
2019-11-03 21:25:49 +0100

    added news on 4days of 4diac

---
+++ news/en_news_20191103.php
@@ -0,0 +1,24 @@
+<?php printNewsEntryHeader(__FILE__, "One Week Till the 4days of 4diac Summit", "img/uws.png"); ?>
+
+<p>In roughly a week the doors will open for the 4days of 4diac summit. 
+For the first time the 4diac community is meeting in such a form.
+Based on that feedback from the attendees whe have identified first topics to be discussed and work on:</p>
+
+<ul>
+	<li>What are the new features of 4diac introduced in the last releases</li>
+	<li>Execution performance and real-time execution of 4diac FORTE</li>
+	<li>Usability of 4diac IDE, we have a moderator with expertise on that topic for Wednesday Nov 13<sup>th</sup></li>
+	<li>4diac on Lego Mindstorms EV3</li>
+	<li>4diac and the Arrowhead Framework</li>
+	<li>Teaching IEC 61499 with Eclipse 4diac</li>
+	<li>...</li>
+</ul>
+
+<p>But there is plenty of space for more!</p>
+
+<p>For the spontaneous people: We still have places left! 
+You can register <a href="https://www.eclipse.org/4diac/en_eve.php?event=2019_4days">here</a>.</p>
+
+<p>Looking forward to meet you next week in Linz!
+<br>The 4diac Team</p>
+<br>
