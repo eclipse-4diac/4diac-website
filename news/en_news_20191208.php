2612182b690e5374c4048eff309f484307caecf8
Alois Zoitl <alois.zoitl@gmx.at>
2019-12-08 19:14:18 +0100

    added news on 4days of 4diac

---
+++ news/en_news_20191208.php
@@ -0,0 +1,29 @@
+<?php printNewsEntryHeader(__FILE__, "4days of 4diac Summary", "img/uws.png"); ?>
+
+<p>From November 11.-14. 2019 more then 20 4diac users and developers from all over the world came together in Linz for the first 4days of 4diac summit. 
+In these four days we had lots of discussions and exchange. 
+To summarize we had the following sessions:</p>
+
+<ul>
+	<li>Improving the 4diac FORTE build and deployment process</li>
+	<li>Several application design sessions, many thanks to Vishnu Easwaran for bringing teaching hardware along
+		<br/> <img width="700" alt="lego mindstorms session" src="events/4daysof4diac/application.jpg">
+		</li>
+	<li>How to use and configure <a href="https://open62541.org/">OPC UA</a> in 4diac applications</li>
+	<li>4diac on <a href="https://www.lego.com/en-us/themes/mindstorms">Lego Mindstorms EV3</a> hack day
+		<br/> <img width="700" alt="lego mindstorms session" src="events/4daysof4diac/lego_session.jpg">
+		</li>
+	<li>How to connect 4diac to the <a href="https://www.arrowhead.eu/arrowheadframework/this-is-it">Arrowhead Framework</a></li>
+	<li>An intense session on usability with first results documented <a href="https://wiki.eclipse.org/Eclipse_4diac_Wiki/4days_Of_4diac/Usability_Session_2019">here</a>
+		<br/> <img width="700" alt="usability session" src="events/4daysof4diac/usability.jpeg">
+		</li>
+	<li>How to implement drivers for the new modular IO interface
+		<br/> <img width="700" alt="io development session" src="events/4daysof4diac/io_session.jpg">
+	</li>
+</ul>
+
+<p>Based on these great results we are look forward to the next 4days of 4diac. 
+Be also prepared for great improvements of the 4diac IDE. 
+We will keep you updated.</p>
+
+<?php include 'greeting.html';?>
