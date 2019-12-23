02b99713228f720d2052396566885bd8917c2f29
Alois Zoitl <alois.zoitl@gmx.at>
2019-12-23 08:08:18 +0100

    added news on 1.11.3 release

---
+++ news/en_news_20191223.php
@@ -0,0 +1,9 @@
+<?php printNewsEntryHeader(__FILE__, "4diac 1.11.3: Third Maintenance Release for 4diac 1.11", "img/release.png"); ?>
+
+<p>We are proud to announce the third maintenance release for Eclipse 4diac 1.11 providing 18 bug fixes for 4diac IDE.</p> 
+
+<p>As always please download, test, and report problems and bugs. 
+For 1.10.x and 1.11.x users a simple <i><q>Help -> Check of Updates</q></i> 
+should upgrade you to 1.11.3.</p>
+
+<?php include 'greeting.html';?>
