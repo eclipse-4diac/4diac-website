7a3bd960f7fe543ac7ddc82e334475d974a36289
Alois Zoitl <alois.zoitl@gmx.at>
2020-03-09 21:03:04 +0100

    added news on agent summer school

---
+++ news/en_news_20200903.php
@@ -0,0 +1,12 @@
+<?php printNewsEntryHeader(__FILE__, "Eclipse 4diac @ International Summer School on Industrial Agents"); ?>
+
+<p>We are very happy to announce that at the 2020 
+<a href="https://warwick.ac.uk/fac/sci/wmg/research/digital/automation/about/summer-school-2020/" >International Summer School on Industrial Agents: 
+Engineering of Cyber-Physical Production Systems</a> will feature to lecture units on IEC 61499. 
+The Eclipse 4diac commiter <a href="https://www.ait.ac.at/en/about-the-ait/researcher-profiles/?tx_aitprofile_pi1%5Bname%5D=Strasser%20Thomas" >Thomas Strasser</a> will give an introductory course on the models of IEC 61499. 
+This is followed by a practical hands-on session on how to implement IEC 61499 solutions with Eclipse 4diac, by the Eclipse 4diac commiter 
+<a href="https://www.jku.at/en/linz-institute-of-technology/research/research-labs/cyber-physical-systems-lab/our-team/univ-prof-di-dr-alois-zoitl/">Alois Zoitl</a>.
+For details on the program and registration information please have a look at the website of the <a href="https://warwick.ac.uk/fac/sci/wmg/research/digital/automation/about/summer-school-2020/" >summer school</a>
+</p>. 
+
+<?php include 'greeting.html';?>
