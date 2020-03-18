f357c80bbe80ef4d32be8078d9a7bf3a45cc456f
Alois Zoitl <alois.zoitl@gmx.at>
2020-03-18 12:35:12 +0100

    fixed issue in last news, added news on bianca

---
+++ news/en_news_20200318.php
@@ -0,0 +1,26 @@
+<?php printNewsEntryHeader(__FILE__, "Eclipse 4diac Welcomes a New Committer"); ?>
+
+<table>
+	<tr>
+	<td width="70%" style='padding-right:10px;'>
+<p>Hi!</p>
+<p>During the past year, 4diac IDE has kept me increasingly busy in my work and private life.
+I got to know the 4diac community - especially during the last 4days of 4diac - and the development process.</p> 
+
+<p>My main focus is currently on improving the usability of the IDE. 
+This greatly improves the learnability of the tool, while also increasing the development speed of experienced developers.
+Furthermore, I am involved in teaching IEC 61499 and Eclipse 4diac at university.</p>
+
+	</td>
+	<td width="30%" style='padding-right:10px;'>
+	<br/> <br/>
+		<img src="news/figs/bianca_wiesmayr.jpg" alt="Bianca Wiesmayr">							
+	</td>
+	</tr>
+</table>
+
+<p>Today, I am excited and honored to announce that I got promoted to committer status.
+In addition to my own developments, I will now increasingly review code submitted by other contributors.</p>
+
+<p>Cheers,<br/>
+Bianca Wiesmayr</p><br>
