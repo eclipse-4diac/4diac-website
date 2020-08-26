02eceb60a031d367b6b466850cc1999445aa63d9
Bianca Wiesmayr <bianca.wiesmayr@jku.at>
2020-08-26 17:31:51 +0200

    news entry: new ecc design
    Change-Id: I5417a71839eeed3ba54bbe07026f35cf7137e99d
    Signed-off-by: Bianca Wiesmayr <bianca.wiesmayr@jku.at>

---
+++ news/en_news_20200826.php
@@ -0,0 +1,12 @@
+<?php printNewsEntryHeader(__FILE__, "Eclipse 4diac 1.13 - Sneak Preview #1"); ?>
+<p>Dear 4diac Users!</p>
+<p>Our next release (1.13) is scheduled for October. As a sneak preview, please have a look at our redesigned ECC.
+To make working with 4diac IDE (even) more fun, we have been modernizing the appearance of your models.</p>
+<img alt="New ECC" width="700" src="news/figs/ecc2020.png">
+<p>With the new transparent background of transition labels, a more compact design of ECCs is feasible. 
+We hope that you also like our new color scheme where output events are shown in red - like the event interface pins. </p>
+
+<p>Cheers,</p>
+<p>Bianca Wiesmayr<br />(4diac Comitter)</p>
+<br/>
+
