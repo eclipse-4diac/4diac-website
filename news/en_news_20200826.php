45307522fb0a37e13ed701726c575c3cf48c480d
Bianca Wiesmayr <bianca.wiesmayr@jku.at>
2020-08-26 17:51:28 +0200

    news entry fix: ECC
    Change-Id: If69e99df99db80f51d2e6925d2a902e501906f81

--- news/en_news_20200826.php
+++ news/en_news_20200826.php
@@ -2,7 +2,7 @@
 <p>Dear 4diac Users!</p>
 <p>Our next release (1.13) is scheduled for October. As a sneak preview, please have a look at our redesigned ECC.
 To make working with 4diac IDE (even) more fun, we have been modernizing the appearance of your models.</p>
-<img alt="New ECC" width="700" src="news/figs/ecc2020.png">
+<img alt="New ECC" width="700" src="news/figs/ecc2020.PNG">
 <p>With the new transparent background of transition labels, a more compact design of ECCs is feasible. 
 We hope that you also like our new color scheme where output events are shown in red - like the event interface pins. </p>
 
