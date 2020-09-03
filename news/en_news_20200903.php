fd6797054abe0f4460e48d365d8eac35c657f321
Bianca Wiesmayr <bianca.wiesmayr@jku.at>
2020-09-03 13:54:42 +0200

    sneak preview 2: datatype editing
    Change-Id: I29a40208c4c22cc0e1825215c9b4b47db72507b3
    Signed-off-by: Bianca Wiesmayr <bianca.wiesmayr@jku.at>

---
+++ news/en_news_20200903.php
@@ -0,0 +1,15 @@
+<?php printNewsEntryHeader(__FILE__, "Eclipse 4diac 1.13 - Sneak Preview #2"); ?>
+<p>Dear 4diac Users!</p>
+<p>The next release of 4diac IDE comes with several major features to improve the support for Structured Datatypes. To be able to easily create and edit structured data types, we decided to introduce the new data type editor. Have a look at it here:</p>
+<img alt="Datatype Editor" width="700" src="news/figs/data_type_editor.PNG">
+<p> 
+With a table layout and direct editing, it is almost self-explanatory and - hopefully - very easy to use. 
+Although we know that a detailed view like a table has the disadvantage of being somewhat confusing, especially with numerous data types, we decided to go for it and tackle this problem head on. 
+The solution we came up with is a new dropdown field:</p>
+<img alt="Dropdownfield with suggestions" width="700" src="news/figs/data_type_dropdown.PNG">
+<p>It is a search field with content proposal and the possibility to open a selection dialog, enabling you to filter through thousands of data types without breaking a sweat.</p>
+
+<p>Cheers,</p>
+<p>Daniel Lindhuber<br />(4diac Developer)</p>
+<br/>
+
