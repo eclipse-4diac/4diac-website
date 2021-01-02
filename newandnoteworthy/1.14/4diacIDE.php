26b4cf1fd3b77c6cc4a25ac9aab66bbdafdf1b42
Alois Zoitl <alois.zoitl@gmx.at>
2021-01-02 18:08:28 +0100

    added new and noteworthy template for 1.14 release
    Signed-off-by: Alois Zoitl <alois.zoitl@gmx.at>

---
+++ newandnoteworthy/1.14/4diacIDE.php
@@ -0,0 +1,52 @@
+<?php include '../nan_helper.php';
+printNaNIDEHeader();
+?>
+	
+<ul>
+	<li><a href="#applicationEditing">Application Editing</a>
+	<li><a href="#fbtEditing">Function Block Type Editing</a>
+	<li><a href="#generalUpdates">General Updates</a>
+</ul>
+	
+	
+	
+<h2 id="applicationEditing">Application Editing</h2>
+<table class=nan>
+	<tr>
+		<td>
+			Lorem Ipsum
+		</td>
+		<td>
+		    Lorem Ipsum
+		</td>
+	</tr>	
+</table>	
+
+<h2 id="fbtEditing">Type Editing</h2>
+<table class=nan>
+	<tr>
+		<td>
+			Lorem Ipsum
+		</td>
+		<td>
+		    Lorem Ipsum
+		</td>
+	</tr>
+
+</table>
+
+<h2 id="generalUpdates">General Updates</h2>
+
+<table class=nan>
+	<tr>
+		<td>
+			Update to Eclipse 2020-12
+		</td>
+		<td>
+		    4diac IDE has been updated to use Eclipse Platform 2020-12.
+		</td>
+	</tr>
+</table>
+
+
+<?php printNaNEntryfooter()?> 
