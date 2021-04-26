6faf8b5da46e7fba93d53f4de083d5693c8b95bf
Alois Zoitl <alois.zoitl@gmx.at>
2021-04-26 22:32:06 +0200

    news entry to scaling graphics fix

---
+++ news/en_news_20210426.php
@@ -0,0 +1,12 @@
+<?php printNewsEntryHeader(__FILE__, "On Our Way to 4diac 1.14.0!", "img/release.png"); ?>
+
+<p>Your patience is paying of. 
+In the last weeks we could fix a few <a href="https://projects.eclipse.org/projects/iot.4diac/releases/1.14.0/bugs">major issues</a> in 4diac IDE. 
+One clearly visible example you can see below. 
+Thanks to Bianca's detective skills we where finally able to fix the cut of texts when zooming in or out of our graphical editors.
+</p>
+
+<?php include 'greeting.html';?>
+
+<img alt="Before and after the new scaling code" width="700" src="news/figs/newscaledgraphics.png">
+<br/>
