13343f64233bab627a1413e30c325125545fc3ff
Alois Zoitl <alois.zoitl@gmx.at>
2022-07-08 15:03:58 +0200

    added news post on 15 years of 4diac
    Change-Id: I2248dc772f81b300d4ec37d503364681c0f20e07

---
+++ news/en_news_20220707.php
@@ -0,0 +1,42 @@
+<?php printNewsEntryHeader(__FILE__, "15 Years of Eclipse 4diac!"); ?>
+
+
+<figure class="single">
+<img src="https://www.eclipse.org/4diac/news/figs/4diac_cake.png" width="150px" alt="Eclipse 4diac's birthday cake" />
+</figure>
+
+
+<p>It is hard to believe that 15 years have passed since on July 7<sup>th</sup>, 
+2007 the first public 4diac version was presented by Thomas Strasser and Alois Zoitl at the <a href="http://www.ieee-ies.org/">IEEE IES</a> International Conference on Industrial Informatics. 
+</p>
+
+<p>In these 15 years, the development of Eclipse 4diac had its ups and downs - but nowadays it is used by universities and research institutions all over the globe. 
+And, increasingly, even companies are using or investigating 4diac for their use cases. 
+<a href="https://www.acin.tuwien.ac.at/en/staff/mmm/">Martin Melik-Merkumians (ACIN, TU Wien)</a>, a long time Eclipse 4diac committer, summarizes our history: 
+<i><q>Being part of the team since 2009, I (well, probably all of us) had the high hopes to make a significant impact to the automation industry and its users with 4diac. 
+Although I had never given up this hope, it didn't always look like it in all these years. 
+All the more it is pleasing to see now, how we managed to change minds in academia and industry.</q></i></p>
+
+<p>
+Already 7 years ago (in 2015), our proposal for an official Eclipse project was accepted. While some developers retired, new ones joined. Currently, we have 8 committers - and many more contributors! 
+<i><q>In the past 3.5 years, 4diac IDE has shaped my work, my PhD studies, and my free time: I deeply enjoy developing new features that benefit users of the IDE. 
+I am happy to be part of the development community, which welcomed me warmly and is a great network of support.</q></i>, 
+<a href="https://www.jku.at/lit-cyber-physical-systems-lab/ueber-uns/team/di-bianca-wiesmayr-bsc/">Bianca Wiesmayr (LIT CPS Lab, JKU)</a>. 
+</p>
+
+<p>
+In the last years, the maturity of Eclipse 4diac has increased tremendously. 
+We implemented numerous improvements and usability enhancements that we did not even dare to dream of 15 years ago. 
+This would not been possible without the feedback from our users. 
+<a href="https://www.jku.at/lit-cyber-physical-systems-lab/ueber-uns/team/univ-prof-di-dr-alois-zoitl/">Alois Zoitl (LIT CPS Lab, JKU)</a>, 
+one of the co-founders and project lead, points it out very nicely: 
+<i><q>Many thanks for the feedback from our users and for the fruitful discussion sessions we had in the last 15 years. 
+I am looking forward to many more constructive discussions for further improvements to turn Eclipse 4diac into the best IEC 61499 solution.</q></i>
+</p>
+
+<p>
+Hence, we are looking forward to the next 15 years of Eclipse 4diac!
+</p>
+
+<?php include 'greeting.html';?>
+
