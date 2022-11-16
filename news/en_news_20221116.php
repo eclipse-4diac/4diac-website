a7e8ad13f2ae2e3de41a10074bbb3ee6158e02e5
Alois Zoitl <alois.zoitl@gmx.at>
2022-11-16 23:35:35 +0100

    Added news on podcast coverage of 4diac

---
+++ news/en_news_20221116.php
@@ -0,0 +1,17 @@
+<?php printNewsEntryHeader(__FILE__, "Eclipse 4diac in Other News"); ?>
+
+<p>Eclipse 4diac was featurd twice in the last two month in the german pod cast 
+&quot;<a href="https://opensourcepodcast.podigee.io/">Open Source in der Industrie</a>&quot;:
+</p>
+
+<ul>
+  <li> Interview with our project leader Alois Zoitl: <a href="https://opensourcepodcast.podigee.io/20-heiligegral">The Holy Grail</a></li>
+  <li> Discussion and retrospective of the SPS 2022 trade fair: <a href="https://opensourcepodcast.podigee.io/22-spsrussland">The PLC of the Russians</a> </li>
+</ul>
+
+<p>
+Many thanks to Dr. Julian Feinauer und Robert Weber for this opportunity.
+</p>
+
+<?php include 'greeting.html';?>
+
