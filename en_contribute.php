14157942d072f7e9038cfd22732ddda16f24e8e1
Alois Zoitl <alois.zoitl@gmx.at>
2022-11-30 21:02:41 +0100

    Added contribution guide to the contribute page

--- en_contribute.php
+++ en_contribute.php
@@ -42,6 +42,8 @@
 			<li>Extend and improve FB library. Having a large set of FBs increases the usability of 4diac for developing control solutions.</li>
 			<li>For ideas where to start your effort have a look at the bugs and features submitted to the <a href="https://bugs.eclipse.org/bugs/buglist.cgi?list_id=13385396&product=4DIAC&query_format=advanced">4diac's issue tracker</a>.</li>
 			<li>For getting started feel free to ask questions in the <a href="http://eclipse.org/forums/eclipse.4diac">4diac discussion forum</a> and in <a href="https://dev.eclipse.org/mailman/listinfo/4diac-dev">4diac's mailing list</a>.</li>
+			<li>A detailed description on how to contribute code to Eclipse 4diac can be found in our <a href="en_help.php?helppage=html/development/contribute.html">contribution guide</a>.
+			
 		</ul>
 		<br>	
 
