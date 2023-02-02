3696cfce8bc39711eef5f0618338e478aa3583d4
Alois Zoitl <alois.zoitl@gmx.at>
2023-02-02 21:08:04 +0100

    added arc industry forum news

---
+++ news/en_news_20230202.php
@@ -0,0 +1,13 @@
+<?php printNewsEntryHeader(__FILE__, "See Eclipse 4diac Live at ARC Industry Forum Americas 2023"); ?>
+
+<p>Eclipse 4diac was already featured at previous the ARC Industry Forums. 
+But this year Eclipse 4diac developers and user will be there to show how they use and how Eclipse 4diac can help you.
+</p>
+
+<p>If you are there, attend the <a href="https://www.arcweb.com/events/arc-industry-forum-orlando/agenda-2023/industrial-open-source-software">Industrial Open Source Software</a>
+Session on Wendesday Feb 8th to see as live! 
+
+This is not only great for Eclipse 4diac but is in general a great message for Open Source Software for Industrial use.
+
+<?php include 'greeting.html';?>
+
