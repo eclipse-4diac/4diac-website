6917b543424ce88d4ddff9cc066cab0bb15701e5
oberlehner <127855312+oberlehner@users.noreply.github.com>
2023-03-14 15:39:45 +0100

    Create en_news_20230314.php

---
+++ news/en_news_20230314.php
@@ -0,0 +1,12 @@
+<?php printNewsEntryHeader(__FILE__, "New comitter on board"); ?>
+
+<p>Dear 4diac community,
+</p>
+
+<p>It's always good to see passionate individuals join open source projects like Eclipse 4diac. 
+As a new committer of Eclipse 4diac, Michael will now have the ability to directly contribute to the development of the project and help guide its future direction. 
+It's also great that he's enthusiastic about supporting the community - open source projects thrive on the support and contributions of their users. 
+We are sure Michael's contributions will be greatly appreciated by the Eclipse 4diac community.</p>
+
+
+<?php include 'greeting.html';?>
