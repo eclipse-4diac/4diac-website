d9b175632d9b9822946f917af1308a355372c8d4
bwiesmayr <65956373+bwiesmayr@users.noreply.github.com>
2023-05-10 14:58:17 +0200

    Add files via upload

---
+++ news/en_news_20230510.php
@@ -0,0 +1,26 @@
+<?php printNewsEntryHeader(__FILE__, "Welcoming our 2023 GSoC contributors"); ?>
+
+<p>Dear 4diac community,
+</p>
+
+<p>
+Every year, Google provides funds to students and beginners of open source to learn about contributing to open source projects. 
+We are pleased that two of the proposals submitted for Eclipse 4diac were accepted. 
+The projects allow students to concentrate on open source contributions during their summer break, and provide a unique opportunity for projects like Eclipse 4diac to develop prototypes for new features.
+</p>
+
+<p>
+	Andrea, a second-year computer science student, will work on improving the quality of the IDE. Paul was already a participant of the program in 2022. This year, he will work on providing tools for developers of IEC 61499 applications to ensure their correctness.
+</p>
+
+<p>
+Two of our committers will serve as mentors for our students. 
+Many thanks to Bianca and Michael for introducing Andrea and Paul to our community and providing support. 
+</p>
+
+<p>
+	Please welcome our contributors for 2023!
+</p>
+
+<?php include 'greeting.html';?>
+
