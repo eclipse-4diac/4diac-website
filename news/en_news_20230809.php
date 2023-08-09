efdf83fd67fc20ae90f66984159cd59a33b22c92
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2023-08-09 15:37:04 +0200

    added news on logiBUS®
    Eclipse 4diac™
    
    
    corrected. 
    
    
    typo
    
    
    Greets
    
    
    .

---
+++ news/en_news_20230809.php
@@ -0,0 +1,50 @@
+<?php printNewsEntryHeader(__FILE__, "Guest Post: Eclipse 4diac™ for Agrimechatronics: The Project logiBUS®"); ?>
+
+<p>
+The folks at HR Agrartechnik GmbH have been busy to port Eclipse 4diac™ FORTE to the ESP32 and integrate the ISOBUS (ISO 11783) protocol
+over the last year. 
+</p>
+
+<p><img src="https://www.eclipse.org/4diac/news/figs/LogiBUS_Logo.svg" alt="logiBUS® Logo" width="125"/> is the Fusion of IEC 61499 programming and ISO 11783 compatibiltiy. 
+</p>
+
+<p>ISOBUS, also known as ISO 11783, is a communication protocol standard developed for agricultural equipment to facilitate the exchange of data between different types of farm machinery and implements. The term "ISOBUS" stands for "ISO Bus," where ISO refers to the International Organization for Standardization, and "Bus" refers to a system of communication between multiple devices.
+</p>
+
+<p>The goal of ISOBUS is to enable interoperability and compatibility among various agricultural devices from different manufacturers. This standard allows tractors, combines, sprayers, seeders, and other agricultural implements to communicate and share information seamlessly. This information exchange can include data such as equipment status, operational settings, and control commands.
+</p>
+
+<p>ISOBUS provides a standardized physical connection, usually implemented through a standard connector, and a set of communication protocols that allow different devices to exchange information. This can improve efficiency on the farm by enabling operators to control and monitor various implements from within the tractor cab, reducing the need for multiple displays and control interfaces.
+</p>
+
+<p>The ISOBUS standard defines different layers of communication, such as the physical layer (defining the connector and cabling), the data link layer (defining how data is packaged and transmitted), and the application layer (defining the structure and content of the data exchanged).
+</p>
+
+<p>In summary, ISOBUS (ISO 11783) is a standardized communication protocol used in the agricultural industry to enable different types of farm equipment to communicate and share data, ultimately leading to improved efficiency and ease of use for operators.
+</p>
+
+
+<p>
+While exising ISOBUS Stacks require heavy C or C++ Programming as well as knowlege of both, the Bus Protocol and the Programming API of the corresponding ISOBUS Stack, 
+logiBUS® ease the same a log by covering the Complexity in IEC 61499 Function Blocks. 
+Therefore the Programming of a ISOBUS Universal Terminal Softkey Input in Eclipse 4diac™ is the same as programming a digital input. Nothing new to learn. 
+</p>
+
+<p>
+A corresponding Website is under construction, but first examples are on the Page: 	<li><a href="https://www.logibus.tech/">logiBUS®</a></li>
+</p>
+
+<p>
+And, to support Open Source, some Customers agreed to publish their Projects: <li><a href="https://github.com/Meisterschulen-am-Ostbahnhof-Munchen/4diac-SlurryTanker-sample">Eclipse 4diac™ SlurryTanker sample</a></li>
+</p>
+
+<p>
+There will be a featured Speech about the basics of logiBUS® at the Espressif Developer
+Conference 2023 September 12, 14:30
+see Details: 	<li><a href="https://devcon.espressif.com/">DevCon23</a></li>
+</p>
+
+
+
+Cheers,
+Franz Höpfinger in behalv of HR Agrartechnik GmbH
