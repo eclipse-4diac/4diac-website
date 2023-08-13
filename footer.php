b7d52e833b2ef53bb013c508e31d350e3a702f50
Alois Zoitl <alois.zoitl@gmx.at>
2023-08-13 15:59:31 +0200

    fixed linked in Eclipse 4diac group link

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
 				<td class="center" id="width33">
 					<a href="https://www.youtube.com/channel/UCt0MlNo6Y7dXCZSDACXFi1g"><img src="/4diac/img/appbar.youtube.png" alt="youtube logo"/></a>
 					<a href="https://twitter.com/4diac"><img src="/4diac/img/appbar.social.twitter.png" alt="twitter log"/></a>
-					<a href="https://www.linkedin.com/grp/home?gid=6949091"><img src="/4diac/img/appbar.social.linkedin.png" alt="linkedin logo"/></a>
+					<a href="https://www.linkedin.com/groups/6949091/"><img src="/4diac/img/appbar.social.linkedin.png" alt="linkedin logo"/></a>
 					<a href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=4DIAC"><img src="/4diac/img/appbar.bug.png" alt="bug icon"/></a>
 					<a href="http://eclipse.org/forums/eclipse.4diac"><img src="/4diac/img/appbar.people.multiple.png" alt="forum icon"/></a>
 					<a href="https://chat.eclipse.org/#/room/#eclipse-4diac:matrix.eclipse.org"><img src="/4diac/img/appbar.matrix.png" alt="matrix logo"/></a>
