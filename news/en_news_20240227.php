f4202184bdbbc6898d2ed14cc2c4c1a51be990e3
Franz Höpfinger <franz.hoepfinger.4diac@logibus.tech>
2024-02-29 16:10:33 +0100

    Add guest post about Eclipse 4diac&trade; project logiBUS&reg;, including award details and image.
    A new news entry was added to showcase the Eclipse 4diac&trade; project logiBUS&reg;, which recently won an award for innovative digital farming at a prestigious conference. The entry includes images of the logiBUS&reg; logo and the award, along with a message from Franz Höpfinger on behalf of HR Agrartechnik GmbH.

---
+++ news/en_news_20240227.php
@@ -0,0 +1,21 @@
+<?php printNewsEntryHeader(__FILE__, "Guest Post: Eclipse 4diac&trade; for Agrimechatronics: The Project logiBUS&reg; won a prestigious award "); ?>
+
+
+
+<p><img src="https://eclipse.dev/4diac/news/figs/LogiBUS_Logo.svg" alt="logiBUS&reg; Logo" width="125"/> is the Fusion of IEC 61499 programming and ISO 11783 compatibiltiy. 
+</p>
+
+
+<p>Now logiBUS&reg; won a prestigious award: "Award for innovative digital farming" it was awarded at the 44th Annual Conference at the University of Hohenheim of the Society for Information Technology in Agriculture, Forestry and the Food Industry e.V.
+</p>
+
+
+<img src="https://www.eclipse.org/4diac/news/figs/IMG_20240227_100459_022.jpg" alt="Award" width="125"/>
+
+
+
+<p>Cheers,<br>
+Franz Höpfinger on behalf of HR Agrartechnik GmbH
+</p>
+<br>
+
