57194a8002a49a280e199c7723eb483b19eef99b
Alois Zoitl <alois.zoitl@gmx.at>
2024-04-10 18:05:34 +0200

    updated the most important repo links to github

--- news/2017/en_news_20170514.php
+++ news/2017/en_news_20170514.php
@@ -11,7 +11,7 @@
 		<td width="60%"><p>With our work on 4diac 1.9.0 we are proud to announce that FORTE now supports the uMIC.200 control devices from the company MicroControl. 
 The uMIC.200 is a compact automation controller, which features in addtion to 8 digital IOs also several communication interfaces. 
 Details can be found at the <a href="http://www.microcontrol.net/en/products/control-systems/umic200/">uMIC.200 website</a>.</p>
-<p>For testing grab the latest FORTE version from the development branch of <a href="http://git.eclipse.org/c/4diac/org.eclipse.4diac.forte.git">FORTE's Git repository</a>.			
+<p>For testing grab the latest FORTE version from the development branch of <a href="https://github.com/eclipse-4diac/4diac-forte">FORTE's Git repository</a>.			
 </p>
 <?php include 'greeting.html';?>
 </td>
