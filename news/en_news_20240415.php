3a083b9975a6d3f47822ea60c2f15023c2191efb
Alois Zoitl <alois.zoitl@gmx.at>
2024-04-15 13:41:34 +0200

    Announcment of RevPi support.

---
+++ news/en_news_20240415.php
@@ -0,0 +1,22 @@
+<?php printNewsEntryHeader(__FILE__, "Eclipse 4diac Now Supports RevolutionPI by Kunbus!"); ?>
+
+<p>
+We are thrilled to announce a significant enhancement to our open-source project: 
+Eclipse 4diac now supports the <a href="https://revolutionpi.com/en/">RevolutionPI</a> device manufactured by <a href="https://www.kunbus.com/en/">Kunbus GmbH</a>. 
+This integration marks a major milestone in our mission to provide comprehensive support for a wide range of industrial devices, empowering developers to build advanced automation and control systems with ease.
+</p>
+<p>
+With this latest addition, Eclipse 4diac users can seamlessly integrate <a href="https://revolutionpi.com/en/">RevolutionPI</a> devices into their automation solutions, harnessing the full potential of Eclipse 4diac's powerful features and functionalities. 
+Whether you're working on industrial control systems, process automation, or IoT applications, Eclipse 4diac now offers enhanced compatibility and support for RevolutionPI devices, empowering you to bring your automation projects to life with unparalleled ease and efficiency.
+</p>
+<p>
+To get started with Eclipse 4diac and RevolutionPI support, simply download the latest development version of 4diac FORTE from our <a href"https://github.com/eclipse-4diac/4diac-forte/tree/develop">Github repository</a> and explore the possibilities today.
+</p>
+<p>
+Thank you for your continued support and enthusiasm for Eclipse 4diac. 
+We look forward to seeing the innovative automation solutions our community will create with RevolutionPI support!
+</p>
+
+<?php include 'greeting.html';?>
+
+
