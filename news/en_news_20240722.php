651986c8362ef14e0387f46e86d72b5193e75a5d
Pedro Ricardo C. Souza <pricardo@aimirimsti.com.br>
2024-07-22 14:14:31 -0300

    feat(news): Insert UAO-RT plugin news.

---
+++ news/en_news_20240722.php
@@ -0,0 +1,27 @@
+<?php printNewsEntryHeader(__FILE__, "New 4diac Plugin by Aimirim enables interoperability with Universal Automation"); ?>
+
+<p>
+<a href="https://www.aimirimsti.com.br/en/solu%C3%A7%C3%B5es">Aimirim</a> would like to anounce what we consider a nice contribution to 4DIAC community.
+The <a href="https://github.com/Aimirim-STI/4diac-Plugin-UAORT">UAORT plug-in</a> is a lighhouse toward interoperability in IEC61499 allowing 4DIAC users deploy on UAO (Universal Automation) runtime.
+</p>
+<p>
+We understood that UAO is growing and the demand for this interoperability is growing also.
+Aimirim has deployed several complex real world industrial applications using 4DIAC what allowed us to understand the advantanges of event driven distributed applications and opensource models to speed up industry 4.0 revolution.
+This experience also make it easier for us to do this job and contribute with the comunity. 
+</p>
+<p>
+That's why we did that, focused on the main goal of unlocking the digitization of industrial operations.
+Aimirim believes that the growth of this market will drive more opportunities to startups and new talents.
+Contributing to 4DIAC also represents this belief.
+</p>
+
+<p>Cheers,
+	<br>Aimirim Team </p>
+<br>
+
+
+
+
+
+
+
