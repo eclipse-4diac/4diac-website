be2b589e913144bae6789999a0238844059f1f54
Alois Zoitl <alois.zoitl@gmx.at>
2024-07-23 22:59:30 +0200

    Improved correct usage of Eclipse 4diac and 4diac IDE.
    Corrected some wording. Especially using configurabilty which is the
    term IEC 61499 uses for the task you implemented.

--- news/en_news_20240722.php
+++ news/en_news_20240722.php
@@ -1,18 +1,19 @@
-<?php printNewsEntryHeader(__FILE__, "New 4diac Plugin by Aimirim enables interoperability with Universal Automation"); ?>
+<?php printNewsEntryHeader(__FILE__, "New 4diac IDE Plugin by Aimirim Enables Configurabilty with Universal Automation Devices"); ?>
 
 <p>
-<a href="https://www.aimirimsti.com.br/en/solu%C3%A7%C3%B5es">Aimirim</a> would like to anounce what we consider a nice contribution to 4DIAC community.
-The <a href="https://github.com/Aimirim-STI/4diac-Plugin-UAORT">UAORT plug-in</a> is a lighthouse toward interoperability in IEC61499 allowing 4DIAC users deploy on UAO (Universal Automation) runtime.
+<a href="https://www.aimirimsti.com.br/en/solu%C3%A7%C3%B5es">Aimirim</a> would like to anounce what we consider a nice contribution to Eclipse 4diac community.
+The <a href="https://github.com/Aimirim-STI/4diac-Plugin-UAORT">UAORT plug-in</a> is a lighthouse towards configurabilty in IEC 61499 by allowing 4diac IDE users deploy to the UAO (Universal Automation) run-time environment.
 </p>
 <p>
-We understood that UAO is growing and the demand for this interoperability is growing also.
-Aimirim has deployed several complex real world industrial applications using 4DIAC what allowed us to understand the advantanges of event driven distributed applications and opensource models to speed up industry 4.0 revolution.
-This experience also make it easier for us to do this job and contribute with the comunity. 
+We understood that UAO is growing and the demand for this configurabilty is growing also.
+Aimirim has deployed several complex real world industrial applications using Eclipse 4diac. 
+This allowed us to understand the advantanges of event driven distributed applications and opensource models to speed up industry 4.0 revolution.
+This experience also make it easier for us to do this job and contribute to the Eclipse 4diac comunity. 
 </p>
 <p>
 That's why we did that, focused on the main goal of unlocking the digitization of industrial operations.
 Aimirim believes that the growth of this market will drive more opportunities to startups and new talents.
-Contributing to 4DIAC also represents this belief.
+Contributing to Eclipse 4diac also represents this belief.
 </p>
 
 <p>Cheers,
