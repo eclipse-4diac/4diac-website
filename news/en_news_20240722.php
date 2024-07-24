7e6d52d423ab428b84c1d37693ded4dbca2ee97e
Pedro Ricardo C Souza <pedro_ricardo9@hotmail.com>
2024-07-24 11:32:04 -0300

    News post on Aimirim 4diac Plugin for UAO-RT (#24)
    * feat(news): Insert UAO-RT plugin news.
    
    * fix(news): Correct typo on news_20240722.
    
    * Improved correct usage of Eclipse 4diac and 4diac IDE.
    
    Corrected some wording. Especially using configurabilty which is the
    term IEC 61499 uses for the task you implemented.
    
    ---------
    
    Co-authored-by: Alois Zoitl <alois.zoitl@gmx.at>

---
+++ news/en_news_20240722.php
@@ -0,0 +1,28 @@
+<?php printNewsEntryHeader(__FILE__, "New 4diac IDE Plugin by Aimirim Enables Configurabilty with Universal Automation Devices"); ?>
+
+<p>
+<a href="https://www.aimirimsti.com.br/en/solu%C3%A7%C3%B5es">Aimirim</a> would like to anounce what we consider a nice contribution to Eclipse 4diac community.
+The <a href="https://github.com/Aimirim-STI/4diac-Plugin-UAORT">UAORT plug-in</a> is a lighthouse towards configurabilty in IEC 61499 by allowing 4diac IDE users deploy to the UAO (Universal Automation) run-time environment.
+</p>
+<p>
+We understood that UAO is growing and the demand for this configurabilty is growing also.
+Aimirim has deployed several complex real world industrial applications using Eclipse 4diac. 
+This allowed us to understand the advantanges of event driven distributed applications and opensource models to speed up industry 4.0 revolution.
+This experience also make it easier for us to do this job and contribute to the Eclipse 4diac comunity. 
+</p>
+<p>
+That's why we did that, focused on the main goal of unlocking the digitization of industrial operations.
+Aimirim believes that the growth of this market will drive more opportunities to startups and new talents.
+Contributing to Eclipse 4diac also represents this belief.
+</p>
+
+<p>Cheers,
+	<br>Aimirim Team </p>
+<br>
+
+
+
+
+
+
+
